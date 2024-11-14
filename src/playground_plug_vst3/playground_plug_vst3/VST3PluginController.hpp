5a0d4078635c892d818a4c2fe782e200abd7cc5d
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-14 21:30:41 +0100

    15

--- src/playground_plug_vst3/playground_plug_vst3/VST3PluginController.hpp
+++ src/playground_plug_vst3/playground_plug_vst3/VST3PluginController.hpp
@@ -1,7 +1,10 @@
 #pragma once
 #include <public.sdk/source/vst/vsteditcontroller.h>
 
+using namespace Steinberg;
+using namespace Steinberg::Vst;
+
 class VST3PluginController:
-public Steinberg::Vst::EditControllerEx1
+public EditControllerEx1
 {
 };
