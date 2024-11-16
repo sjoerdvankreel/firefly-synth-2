39bc22c63da401d01ae74563d84bd4bc6bb43ba5
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-17 00:10:05 +0100

    67

--- src/playground_plug/playground_plug/base/shared/FBPluginBlocks.hpp
+++ src/playground_plug/playground_plug/base/shared/FBPluginBlocks.hpp
@@ -4,18 +4,41 @@
 #include <cstddef>
 
 template <int Count>
-using FBSingleBlock = std::array<float, Count>;
-template <int Count>
-using FBDualBlock = std::array<FBSingleBlock<Count>, 2>;
+struct FBSingleBlock
+{
+  std::array<float, Count> detail_store;
+
+  float& operator[](int index) 
+  { return detail_store[index]; }
+  float const& operator[](int index) const 
+  { return detail_store[index]; }
+
+  void SetToZero() 
+  { detail_store.fill(0.0f); }
+  void InPlaceMultiply(float x) 
+  { for (int i = 0; i < Count; i++) (*this)[i] *= x; }
+  void InPlaceAdd(FBSingleBlock const& rhs) 
+  { for (int i = 0; i < Count; i++) (*this)[i] += rhs[i]; }
+  void CopyTo(float* destination, int count) 
+  { for (int i = 0; i < count; i++) destination[i] = (*this)[i]; }
+};
 
 template <int Count>
-inline int constexpr FBBlockSize(FBSingleBlock<Count> const& block)
+struct FBDualBlock
 {
-  return static_cast<int>(block.size());
-}
+  std::array<FBSingleBlock<Count>, 2> detail_store;
 
-template <int Count>
-inline int constexpr FBBlockSize(FBDualBlock<Count> const& block)
-{
-  return FBBlockSize(block[0]);
-}
+  FBSingleBlock<Count>& operator[](int index) 
+  { return detail_store[index]; }
+  FBSingleBlock<Count> const& operator[](int index) const 
+  { return detail_store[index]; }
+
+  void SetToZero() 
+  { for (int i = 0; i < 2; i++) (*this)[i].SetToZero(); }
+  void InPlaceMultiply(float x) 
+  { for (int i = 0; i < 2; i++) (*this)[i].InPlaceMultiply(x); }
+  void InPlaceAdd(FBDualBlock const& rhs) 
+  { for (int i = 0; i < 2; i++) (*this)[i].InPlaceAdd(rhs[i]); }
+  void CopyTo(float* const* destination, int count) 
+  { for (int i = 0; i < 2; i++) (*this)[i].CopyTo(destination[i]); }
+};
