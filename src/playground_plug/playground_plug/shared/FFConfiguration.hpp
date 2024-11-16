0a917cfb9503a9cba06460354db69b68a0f45311
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-16 15:13:11 +0100

    45

---
+++ src/playground_plug/playground_plug/shared/FFConfiguration.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <playground_plug/shared/FFDefines.hpp>
+
+#define FF_VENDOR_NAME "Sjoerd van Kreel"
+#define FF_VENDOR_MAIL "sjoerdvankreel@gmail.com"
+#define FF_VENDOR_URL "https://github.com/sjoerdvankreel"
+
+#define FF_PLUGIN_NAME "Firefly Synth"
+#define FF_PLUGIN_PROCESSOR_ID "754068B351A04DB4813B58D562BDFC1F"
+#define FF_PLUGIN_CONTROLLER_ID "959E6302402B461A8C9AA5A6737BCAAD"
+#define FF_PLUGIN_VERSION FF_STRINGIFY(FF_PLUGIN_VERSION_MAJOR.FF_PLUGIN_VERSION_MINOR.FF_PLUGIN_VERSION_PATCH)
--- src/playground_plug_vst3/playground_plug_vst3/FFVST3PluginFactory.cpp
+++ src/playground_plug_vst3/playground_plug_vst3/FFVST3PluginFactory.cpp
@@ -1,6 +1,7 @@
+#include <playground_plug/shared/FFConfiguration.hpp>
 #include <playground_plug_vst3/FFVST3PluginProcessor.hpp>
 #include <playground_plug_vst3/FFVST3PluginController.hpp>
-#include <playground_plug/shared/FFDefines.hpp>
+
 #include <public.sdk/source/main/pluginfactory.h>
 #include <pluginterfaces/vst/ivstcomponent.h>
 #include <pluginterfaces/vst/ivsteditcontroller.h>
@@ -20,13 +21,13 @@ TextToFUID(char const* text)
 static FUID
 ControllerFUID()
 {
-  return TextToFUID(FF_SYNTH_CONTROLLER_ID);
+  return TextToFUID(FF_PLUGIN_CONTROLLER_ID);
 }
 
 static FUID
 ProcessorFUID()
 {
-  return TextToFUID(FF_SYNTH_PROCESSOR_ID);
+  return TextToFUID(FF_PLUGIN_PROCESSOR_ID);
 }
 
 static FUnknown*
@@ -47,11 +48,11 @@ BEGIN_FACTORY_DEF(FF_VENDOR_NAME, FF_VENDOR_URL, FF_VENDOR_MAIL)
   DEF_CLASS2(
     INLINE_UID_FROM_FUID(ProcessorFUID()),
       PClassInfo::kManyInstances, kVstAudioEffectClass, 
-      FF_SYNTH_NAME, kDistributable, PlugType::kInstrument, 
-      FF_VERSION, kVstVersionString, ProcessorFactory);
+      FF_PLUGIN_NAME, kDistributable, PlugType::kInstrument,
+      FF_PLUGIN_VERSION, kVstVersionString, ProcessorFactory);
   DEF_CLASS2(
     INLINE_UID_FROM_FUID(ControllerFUID()),
       PClassInfo::kManyInstances, kVstComponentControllerClass, 
-      FF_SYNTH_NAME, 0, "", 
-      FF_VERSION, kVstVersionString, ControllerFactory)
+      FF_PLUGIN_NAME, 0, "",
+      FF_PLUGIN_VERSION, kVstVersionString, ControllerFactory)
 END_FACTORY
