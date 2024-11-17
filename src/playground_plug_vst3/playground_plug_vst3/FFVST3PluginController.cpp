f5497406d953d04fc3547f3793f01028c7f8ed71
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-17 17:25:01 +0100

    78

--- src/playground_plug_vst3/playground_plug_vst3/FFVST3PluginController.cpp
+++ src/playground_plug_vst3/playground_plug_vst3/FFVST3PluginController.cpp
@@ -1,6 +1,19 @@
 #include <playground_plug/plug/shared/FFPluginTopo.hpp>
 #include <playground_plug_vst3/FFVST3PluginController.hpp>
 
+static UnitInfo
+MakeUnitInfo(FBRuntimeModule const& module, int id)
+{
+  UnitInfo result;
+  result.id = id;
+  result.parentUnitId = kRootUnitId;
+  result.programListId = kNoProgramListId;
+  memset(result.name, 0, sizeof(result.name));
+  for (int i = 0; i < 127 && i < module.name.size(); i++)
+    result.name[i] = module.name[i];
+  return result;
+}
+
 tresult PLUGIN_API
 FFVST3PluginController::initialize(FUnknown* context)
 {
@@ -10,16 +23,6 @@ FFVST3PluginController::initialize(FUnknown* context)
   int unitId = 1; // 0 is root
   auto topo = FBRuntimeTopo(FFMakeTopo());
   for (int m = 0; m < topo.modules.size(); m++)
-  {
-    UnitInfo unitInfo;
-    unitInfo.id = unitId++;
-    unitInfo.parentUnitId = kRootUnitId;
-    unitInfo.programListId = kNoProgramListId;
-    memset(unitInfo.name, 0, sizeof(unitInfo.name));
-    auto const& module = topo.modules[m];
-    for (int i = 0; i < 127 && i < module.name.size(); i++)
-      unitInfo.name[i] = module.name[i];
-    addUnit(new Unit(unitInfo));
-  }
+    addUnit(new Unit(MakeUnitInfo(topo.modules[m], unitId++)));
   return kResultTrue;
 }
