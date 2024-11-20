52fba7389da3d55bb8ab1743ab81430104c01b54
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-20 22:20:04 +0100

    137

--- src/playground_plug/playground_plug/base/FFBlock.hpp
+++ src/playground_plug/playground_plug/base/FFBlock.hpp
@@ -23,12 +23,15 @@ public:
   void SetToZero() 
   { Store().fill(0.0f); }
   void CopyTo(DerivedT& rhs) const
-  { for (int i = 0; i < Store().size(); i++) rhs[i] = (*this)[i]; }
+  { for (int i = 0; i < Store().size(); i++) 
+    rhs[i] = (*this)[i]; }
 
   void InPlaceMultiply(float x) 
-  { for (int i = 0; i < Store().size(); i++) (*this)[i] *= x; }
+  { for (int i = 0; i < Store().size(); i++) 
+    (*this)[i] *= x; }
   void InPlaceAdd(DerivedT const& rhs)
-  { for (int i = 0; i < Store().size(); i++) (*this)[i] += rhs[i]; }
+  { for (int i = 0; i < Store().size(); i++) 
+    (*this)[i] += rhs[i]; }
 };
 
 class FFDynamicMonoBlock:
@@ -50,4 +53,55 @@ public FFMonoBlockMixin<FFFixedMonoBlock>
 
 public:
   FF_NOCOPY_NOMOVE_DEFCTOR(FFFixedMonoBlock);
+};
+
+template <class DerivedT>
+class FFStereoBlockMixin
+{
+  auto& Store() 
+  { return static_cast<DerivedT*>(this)->_store; }
+  auto const& Store() const 
+  { return static_cast<DerivedT const*>(this)->_store; }
+
+public:
+  auto& operator[](int channel) 
+  { return Store()[channel]; }
+  auto const& operator[](int channel) const
+  { return Store()[channel]; }
+
+  void SetToZero() 
+  { for(int ch = 0; ch < FF_CHANNELS_STEREO; ch++) 
+    (*this)[ch].SetToZero(); }
+  void CopyTo(DerivedT& rhs) const
+  { for(int ch = 0; ch < FF_CHANNELS_STEREO; ch++) 
+    (*this)[ch].CopyTo(rhs[ch]); }
+
+  void InPlaceMultiply(float x) 
+  { for(int ch = 0; ch < FF_CHANNELS_STEREO; ch++) 
+    (*this)[ch].InPlaceMultiply(x); }
+  void InPlaceAdd(DerivedT const& rhs)
+  { for(int ch = 0; ch < FF_CHANNELS_STEREO; ch++) 
+    (*this)[ch].InPlaceAdd(rhs[ch]); }
+};
+
+class FFDynamicStereoBlock:
+public FFStereoBlockMixin<FFDynamicStereoBlock>
+{
+  std::array<std::vector<float>, FF_CHANNELS_STEREO> _store;
+  friend class FFStereoBlockMixin<FFDynamicStereoBlock>;
+
+public:
+  FF_NOCOPY_NOMOVE_NODEFCTOR(FFDynamicStereoBlock);
+  FFDynamicStereoBlock(int size) : 
+  _store({ std::vector<float>(size, 0.0f), std::vector<float>(size, 0.0f) }) {}
+};
+
+class alignas(FF_BLOCK_SIZE * sizeof(float)) FFFixedStereoBlock:
+public FFStereoBlockMixin<FFFixedStereoBlock>
+{
+  std::array<std::array<float, FF_BLOCK_SIZE>, FF_CHANNELS_STEREO> _store;
+  friend class FFStereoBlockMixin<FFFixedStereoBlock>;
+
+public:
+  FF_NOCOPY_NOMOVE_DEFCTOR(FFFixedStereoBlock);
 };
--- src/playground_plug/playground_plug/base/foo.cpp
+++ src/playground_plug/playground_plug/base/foo.cpp
@@ -2,17 +2,9 @@
 
 void boo()
 {
-  FFDynamicMonoBlock const xx(90);
-  FFDynamicMonoBlock xx1(91);
-  //xx[90] = 3;
-  float z = xx[0];
-  xx.CopyTo(xx1);
+  FFFixedStereoBlock const xx = {};
+  FFFixedStereoBlock xx1;
 
-  FFFixedMonoBlock const zz;
-  //auto zz1 = zz;
-  zz[4] = 9;
-  zz[99] = 90;
-
-  std::array<float, 1> zork;
-  zork[9] = 3;
+  xx[0][6] = 3;
+  
 }
