b59a3d4de165897ee76eda2773006fcc616163df
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-23 18:26:24 +0100

    172

--- src/playground_plug/playground_plug/base/dsp/FFFixedBlockProcessor.hpp
+++ src/playground_plug/playground_plug/base/dsp/FFFixedBlockProcessor.hpp
@@ -16,12 +16,14 @@ public:
   FFFixedBlockProcessor(int maxHostSampleCount);
 
   template <class Processor>
-  void Process(FFHostInputBlock const& input, FFRawStereoBlock& output, Processor& processor);
+  void Process(
+    FFHostInputBlock const& input, FFRawStereoBlock& output, Processor& processor);
 };
 
 template <class Processor>
 void 
-FFFixedBlockProcessor::Process(FFHostInputBlock const& input, FFRawStereoBlock& output, Processor& processor)
+FFFixedBlockProcessor::Process(
+  FFHostInputBlock const& input, FFRawStereoBlock& output, Processor& processor)
 {
   _inputSplitter.Accumulate(input);
   FFFixedInputBlock const* splitted = nullptr;
