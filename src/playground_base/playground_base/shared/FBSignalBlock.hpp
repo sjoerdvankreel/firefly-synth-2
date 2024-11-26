3226293238749674e4b2998217b0747fc93b339a
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-11-26 19:34:09 +0100

    246

--- src/playground_base/playground_base/shared/FBSignalBlock.hpp
+++ src/playground_base/playground_base/shared/FBSignalBlock.hpp
@@ -10,7 +10,7 @@
 #define FB_CHANNEL_L 0
 #define FB_CHANNEL_R 1
 #define FB_CHANNELS_STEREO 2
-#define FB_FIXED_BLOCK_SIZE 512 // TODO make it good
+#define FB_FIXED_BLOCK_SIZE 16 // TODO make it good
 
 class FBRawBlockView
 {
