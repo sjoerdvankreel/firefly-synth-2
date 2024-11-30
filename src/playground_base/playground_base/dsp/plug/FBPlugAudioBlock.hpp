c2a48dfa9cae3ee28326805df9a7d442189cfa37
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-01 00:45:42 +0100

    309

--- src/playground_base/playground_base/dsp/plug/FBPlugAudioBlock.hpp
+++ src/playground_base/playground_base/dsp/plug/FBPlugAudioBlock.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <playground_base/dsp/shared/FBAnyAudioBlock.hpp>
 #include <playground_base/base/plug/FBPlugConfig.hpp>
 #include <playground_base/base/shared/FBLifetime.hpp>
+#include <playground_base/dsp/shared/FBAnyAudioBlock.hpp>
 
 #include <array>
 
