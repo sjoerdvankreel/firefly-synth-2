182bb9f775f088d0c396176d33c81196f050f5e7
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-04 19:45:33 +0100

    390

--- src/playground_base/playground_base/dsp/pipeline/shared/FBVoiceInfo.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <playground_base/dsp/pipeline/shared/FBNoteEvent.hpp>
-
-struct FBVoiceInfo
-{
-  int index;
-  FBNoteEvent source;
-};
--- src/playground_base/playground_base/dsp/pipeline/shared/FBVoiceManager.hpp
+++ src/playground_base/playground_base/dsp/pipeline/shared/FBVoiceManager.hpp
@@ -2,27 +2,27 @@
 
 #include <playground_base/base/shared/FBLifetime.hpp>
 #include <playground_base/dsp/shared/FBDSPConfig.hpp>
-#include <playground_base/dsp/pipeline/shared/FBVoiceInfo.hpp>
+#include <playground_base/dsp/pipeline/shared/FBNoteEvent.hpp>
 
 #include <array>
 
-struct FBVoiceManagerInfo
+struct FBVoiceInfo
 {
   bool active = {};
-  FBVoiceInfo info = {};
+  FBNoteEvent event = {};
 };
 
 class FBVoiceManager
 {
   std::uint64_t _counter = {};
   std::array<std::uint64_t, FB_MAX_VOICES> _num = {};
-  std::array<FBVoiceManagerInfo, FB_MAX_VOICES> _voices = {};
+  std::array<FBVoiceInfo, FB_MAX_VOICES> _voices = {};
 
 public:
-  void Return(std::uint64_t leased);
-  std::uint64_t Lease(FBNoteEvent const& event);
+  void Return(int leased);
+  int Lease(FBNoteEvent const& event);
 
   FB_NOCOPY_NOMOVE_DEFCTOR(FBVoiceManager);
-  std::array<FBVoiceManagerInfo, FB_MAX_VOICES> const& 
+  std::array<FBVoiceInfo, FB_MAX_VOICES> const&
   Voices() { return _voices; }
 };
