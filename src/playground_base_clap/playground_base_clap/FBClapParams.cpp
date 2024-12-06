c0ca2e63776fc8692a3aac47a4bfa0ddfaa3e248
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-07 00:58:13 +0100

    428

--- src/playground_base_clap/playground_base_clap/FBClapParams.cpp
+++ src/playground_base_clap/playground_base_clap/FBClapParams.cpp
@@ -8,8 +8,16 @@ FBCLAPPlugin::paramsCount() const noexcept
   return static_cast<uint32_t>(_topo->params.size());
 }
 
+void
+FBCLAPPlugin::paramsFlush(
+  const clap_input_events* in, const clap_output_events* out) noexcept
+{
+  // TODO
+}
+
 bool 
-FBCLAPPlugin::paramsValue(clap_id paramId, double* value) noexcept
+FBCLAPPlugin::paramsValue(
+  clap_id paramId, double* value) noexcept
 {
   int32_t index = getParamIndexForParamId(paramId);
   if (index == -1)
@@ -19,120 +27,77 @@ FBCLAPPlugin::paramsValue(clap_id paramId, double* value) noexcept
   return *_statePtrs.single.block[index];
 }
 
-bool 
-FBCLAPPlugin::paramsInfo(uint32_t paramIndex, clap_param_info* info) const noexcept
+bool
+FBCLAPPlugin::paramsValueToText(
+  clap_id paramId, double value, char* display, uint32_t size) noexcept
 {
-  auto const& param = _topo->params[paramIndex];
-  auto const& module = _topo->modules[param.runtimeModuleIndex];
+  int32_t index = getParamIndexForParamId(paramId);
+  if (index == -1)
+    return false;
+  std::string text = _topo->params[index].static_.NormalizedToText(false, value);
+  std::fill(display, display + size, 0);
+  strncpy(display, text.c_str(), text.size() - 1);
+  return true;
+}
+
+bool
+FBCLAPPlugin::paramsTextToValue(
+  clap_id paramId, const char* display, double* value) noexcept
+{
+  int32_t index = getParamIndexForParamId(paramId);
+  if (index == -1)
+    return false;
+  auto normalized = _topo->params[index].static_.TextToNormalized(display, false);
+  if (!normalized.has_value())
+    return false;
+  *value = normalized.value();
+  return true;
+}
+
+bool 
+FBCLAPPlugin::paramsInfo(
+  uint32_t paramIndex, clap_param_info* info) const noexcept
+{
+  auto const& runtimeParam = _topo->params[paramIndex];
+  auto const& staticParam = runtimeParam.static_;
+  auto const& runtimeModule = _topo->modules[runtimeParam.runtimeModuleIndex];
+  auto const& staticModule = _topo->static_.modules[runtimeParam.staticModuleIndex];
   if (paramIndex >= _topo->params.size())
     return false;
 
-  info->id = param.tag;
   info->max_value = 1.0;
   info->min_value = 0.0;
   info->cookie = nullptr;
-  info->default_value = param.static_.defaultNormalized;
+  info->id = runtimeParam.tag;
+  info->default_value = staticParam.defaultNormalized;
   std::fill(info->name, info->name + sizeof(info->name), 0);
   std::fill(info->module, info->module + sizeof(info->module), 0);
-  strncpy(info->module, module.name.c_str(), module.name.size() - 1);
-  strncpy(info->name, param.shortName.c_str(), param.shortName.size() - 1);
+  strncpy(info->module, runtimeModule.name.c_str(), runtimeModule.name.size() - 1);
+  strncpy(info->name, runtimeParam.shortName.c_str(), runtimeParam.shortName.size() - 1);
 
-  info->flags = 0;
-
-#if 0
-
-    // Is this param stepped? (integer values only)
-  // if so the double value is converted to integer using a cast (equivalent to trunc).
-  CLAP_PARAM_IS_STEPPED = 1 << 0,
-
-    // Useful for periodic parameters like a phase
-    CLAP_PARAM_IS_PERIODIC = 1 << 1,
-
-    // The parameter should not be shown to the user, because it is currently not used.
-    // It is not necessary to process automation for this parameter.
-    CLAP_PARAM_IS_HIDDEN = 1 << 2,
-
-    // The parameter can't be changed by the host.
-    CLAP_PARAM_IS_READONLY = 1 << 3,
-
-    // This parameter is used to merge the plugin and host bypass button.
-    // It implies that the parameter is stepped.
-    // min: 0 -> bypass off
-    // max: 1 -> bypass on
-    CLAP_PARAM_IS_BYPASS = 1 << 4,
-
-    // When set:
-    // - automation can be recorded
-    // - automation can be played back
-    //
-    // The host can send live user changes for this parameter regardless of this flag.
-    //
-    // If this parameter affects the internal processing structure of the plugin, ie: max delay, fft
-    // size, ... and the plugins needs to re-allocate its working buffers, then it should call
-    // host->request_restart(), and perform the change once the plugin is re-activated.
-    CLAP_PARAM_IS_AUTOMATABLE = 1 << 5,
-
-    // Does this parameter support per note automations?
-    CLAP_PARAM_IS_AUTOMATABLE_PER_NOTE_ID = 1 << 6,
-
-    // Does this parameter support per key automations?
-    CLAP_PARAM_IS_AUTOMATABLE_PER_KEY = 1 << 7,
-
-    // Does this parameter support per channel automations?
-    CLAP_PARAM_IS_AUTOMATABLE_PER_CHANNEL = 1 << 8,
-
-    // Does this parameter support per port automations?
-    CLAP_PARAM_IS_AUTOMATABLE_PER_PORT = 1 << 9,
-
-    // Does this parameter support the modulation signal?
-    CLAP_PARAM_IS_MODULATABLE = 1 << 10,
-
-    // Does this parameter support per note modulations?
-    CLAP_PARAM_IS_MODULATABLE_PER_NOTE_ID = 1 << 11,
-
-    // Does this parameter support per key modulations?
-    CLAP_PARAM_IS_MODULATABLE_PER_KEY = 1 << 12,
-
-    // Does this parameter support per channel modulations?
-    CLAP_PARAM_IS_MODULATABLE_PER_CHANNEL = 1 << 13,
-
-    // Does this parameter support per port modulations?
-    CLAP_PARAM_IS_MODULATABLE_PER_PORT = 1 << 14,
-
-    // Any change to this parameter will affect the plugin output and requires to be done via
-    // process() if the plugin is active.
-    //
-    // A simple example would be a DC Offset, changing it will change the output signal and must be
-    // processed.
-    CLAP_PARAM_REQUIRES_PROCESS = 1 << 15,
-
-    // This parameter represents an enumerated value.
-    // If you set this flag, then you must set CLAP_PARAM_IS_STEPPED too.
-    // All values from min to max must not have a blank value_to_text().
-    CLAP_PARAM_IS_ENUM = 1 << 16,
-
-    info->name param.shortName
-    //info->flags = clap_param_info_flags
-    //info->module;
-    //info->name;
-
-#endif
-    return true;
-}
-
-bool 
-FBCLAPPlugin::paramsTextToValue(clap_id paramId, const char* display, double* value) noexcept
-{
-  return false;
-}
-
-void 
-FBCLAPPlugin::paramsFlush(const clap_input_events* in, const clap_output_events* out) noexcept
-{
-}
-
-bool 
-FBCLAPPlugin::paramsValueToText(clap_id paramId, double value, char* display, uint32_t size) noexcept
-{
-  return false;
+  // TODO CLAP_PARAM_IS_HIDDEN
+  // TODO CLAP_PARAM_IS_READONLY
+  // TODO CLAP_PARAM_REQUIRES_PROCESS when?
+  info->flags = CLAP_PARAM_REQUIRES_PROCESS;
+  if (staticParam.valueCount != 0)
+  {
+    info->flags |= CLAP_PARAM_IS_STEPPED;
+    if (staticParam.list.size() != 0)
+      info->flags |= CLAP_PARAM_IS_ENUM;
+  }
+  else
+  {
+    info->flags |= CLAP_PARAM_IS_AUTOMATABLE;
+    info->flags |= CLAP_PARAM_IS_MODULATABLE;
+    if (staticModule.voice)
+    {
+      info->flags |= CLAP_PARAM_IS_AUTOMATABLE_PER_KEY;
+      info->flags |= CLAP_PARAM_IS_MODULATABLE_PER_KEY;
+      info->flags |= CLAP_PARAM_IS_AUTOMATABLE_PER_CHANNEL;
+      info->flags |= CLAP_PARAM_IS_MODULATABLE_PER_CHANNEL;
+      info->flags |= CLAP_PARAM_IS_AUTOMATABLE_PER_NOTE_ID;
+      info->flags |= CLAP_PARAM_IS_MODULATABLE_PER_NOTE_ID;
+    }
+  }
+  return true;
 }
