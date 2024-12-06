40b369d58c432b4462eb6fdce3623e7498d89a9d
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-06 22:16:09 +0100

    423

--- src/playground_base_clap/playground_base_clap/FBCLAPPlugin.cpp
+++ src/playground_base_clap/playground_base_clap/FBCLAPPlugin.cpp
@@ -29,39 +29,3 @@ FBCLAPPlugin::getParamInfoForParamId(clap_id paramId, clap_param_info* info) con
 {
   return false;
 }
-
-// TODO move
-
-uint32_t 
-FBCLAPPlugin::paramsCount() const noexcept
-{
-
-}
-
-bool 
-FBCLAPPlugin::paramsValue(clap_id paramId, double* value) noexcept
-{
-
-}
-
-bool 
-FBCLAPPlugin::paramsInfo(uint32_t paramIndex, clap_param_info* info) const noexcept
-{
-
-}
-
-bool 
-FBCLAPPlugin::paramsTextToValue(clap_id paramId, const char* display, double* value) noexcept
-{
-
-}
-
-void 
-FBCLAPPlugin::paramsFlush(const clap_input_events* in, const clap_output_events* out) noexcept
-{
-}
-
-bool 
-FBCLAPPlugin::paramsValueToText(clap_id paramId, double value, char* display, uint32_t size) noexcept
-{
-}
