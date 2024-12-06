366c9dd8fdfca142e1e7c92030e56a81f7bc9e87
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-06 21:36:05 +0100

    420

--- src/playground_base_clap/playground_base_clap/FBCLAPPlugin.cpp
+++ src/playground_base_clap/playground_base_clap/FBCLAPPlugin.cpp
@@ -1,5 +1,24 @@
 #include <playground_base_clap/FBCLAPPlugin.hpp>
+#include <clap/helpers/plugin.hxx>
 
 FBCLAPPlugin::
 FBCLAPPlugin(clap_plugin_descriptor const* desc, clap_host const* host):
 Plugin(desc, host) {}
+
+bool 
+FBCLAPPlugin::isValidParamId(clap_id paramId) const noexcept
+{
+  return false;
+}
+
+int32_t 
+FBCLAPPlugin::getParamIndexForParamId(clap_id paramId) const noexcept
+{
+  return 0;
+}
+
+bool 
+FBCLAPPlugin::getParamInfoForParamId(clap_id paramId, clap_param_info* info) const noexcept
+{
+  return false;
+}
--- src/playground_base_clap/playground_base_clap/FBCLAPPlugin.hpp
+++ src/playground_base_clap/playground_base_clap/FBCLAPPlugin.hpp
@@ -10,4 +10,8 @@ public Plugin<MisbehaviourHandler::Ignore, CheckingLevel::Maximal>
 public:
   FB_NOCOPY_NOMOVE_NODEFCTOR(FBCLAPPlugin);
   FBCLAPPlugin(clap_plugin_descriptor const* desc, clap_host const* host);
+
+  bool isValidParamId(clap_id paramId) const noexcept override;
+  int32_t getParamIndexForParamId(clap_id paramId) const noexcept override;
+  bool getParamInfoForParamId(clap_id paramId, clap_param_info* info) const noexcept override;
 };
