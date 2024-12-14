15724458f09b163bff8c2fd6bff7c05d4db8e1a9
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-14 22:03:15 +0100

    557

--- src/playground_plug/playground_plug/modules/glfo/FFGLFOProcessor.cpp
+++ src/playground_plug/playground_plug/modules/glfo/FFGLFOProcessor.cpp
@@ -1,43 +1,28 @@
 #include <playground_plug/shared/FFPlugTopo.hpp>
 #include <playground_plug/shared/FFPlugState.hpp>
 #include <playground_plug/pipeline/FFModuleProcState.hpp>
-#include <playground_base/base/topo/FBStaticTopo.hpp>
-#include <playground_base/dsp/shared/FBDSPUtility.hpp>
 #include <playground_plug/modules/glfo/FFGLFOProcessor.hpp>
-// TODO
+#include <playground_base/base/topo/FBStaticTopo.hpp>
 
 #include <cmath>
-
-struct FFGLFOBlock
-{
-  FFModuleProcState const& state;
-  FBFixedVectorBlock& output;
-  bool on;
-  FBFixedVectorBlock const& rate;
-};
-
-void 
-FFGLFOProcessor::Generate(FFGLFOBlock& block)
-{
-  for (int s = 0; s < FBFixedBlockSamples; s++)
-    block.output.Sample(s, _phase.Next(block.state.sampleRate, 1.0f));
-  for (int v = 0; v < FBFixedBlockVectors; v++)
-    block.output[v] = (block.output[v] * FBFloatVector::TwoPi()).Sin().Unipolar();
-}
 
 void
 FFGLFOProcessor::Process(FFModuleProcState const& state)
 {
   auto const& topo = state.topo->modules[FFModuleGLFO];
   auto const& params = state.proc->param.global.glfo[state.moduleSlot];
-  FFGLFOBlock block = {
-    state,
-    state.proc->dsp.global.glfo[state.moduleSlot].output,
-    topo.params[FFGLFOBlockOn].NormalizedToBool(params.block.on[0].Value()),
-    params.acc.rate[0].Global().CV() };
+  bool on = topo.params[FFGLFOBlockOn].NormalizedToBool(params.block.on[0].Value());
+  auto& output = state.proc->dsp.global.glfo[state.moduleSlot].output;
+  auto const& rate = params.acc.rate[0].Global().CV();
 
-  if (block.on)
-    Generate(block);
-  else
-    block.output.Clear();
+  if (!on)
+  {
+    output.Clear();
+    return;
+  }
+    
+  for (int s = 0; s < FBFixedBlockSamples; s++)
+    output.Sample(s, _phase.Next(state.sampleRate, 1.0f)); // TODO rate
+  for (int v = 0; v < FBFixedBlockVectors; v++)
+    output[v] = (output[v] * FBFloatVector::TwoPi()).Sin().Unipolar();
 }
