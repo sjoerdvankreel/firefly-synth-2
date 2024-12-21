44b4375e5f1a944321ab8359cff2fbffbced1f7d
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-21 19:17:16 +0100

    630

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -2,13 +2,11 @@
 setlocal
 
 if "%1" == "" goto usage
-if "%2" == "" goto usage
-if "%3" == "" goto usage
 
 cd ..
-if not exist build\windows mkdir build\windows
-cd build\windows
-cmake -D FB_USE_SSE="%2" -D FB_USE_AVX="%3" ..\..
+if not exist build\windows\sse mkdir build\windows\sse
+cd build\windows\sse
+cmake -D FB_USE_SSE=1 -D FB_USE_AVX=0 ..\..\..
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 msbuild /property:Configuration="%1" playground_plug.sln
@@ -17,5 +15,5 @@ if %errorlevel% neq 0 exit /b !errorlevel!
 exit /b 0
 
 :usage
-echo "Usage: Debug|Release FB_USE_SSE=0|1 FB_USE_AVX=0|1. Example: build_windows Debug 1 0 builds debug binaries with SSE support."
+echo "Usage: build_windows Debug|Release"
 exit /b 1
