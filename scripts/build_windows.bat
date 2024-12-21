4385ca240ef587da8b88f42ccf3e941c0d008da2
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-21 19:19:07 +0100

    631

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -2,18 +2,24 @@
 setlocal
 
 if "%1" == "" goto usage
-
 cd ..
+
 if not exist build\windows\sse mkdir build\windows\sse
 cd build\windows\sse
 cmake -D FB_USE_SSE=1 -D FB_USE_AVX=0 ..\..\..
 if %errorlevel% neq 0 exit /b !errorlevel!
+msbuild /property:Configuration="%1" playground_plug.sln
+if %errorlevel% neq 0 exit /b !errorlevel!
 
+cd ..\..\..
+if not exist build\windows\avx mkdir build\windows\avx
+cd build\windows\avx
+cmake -D FB_USE_SSE=0 -D FB_USE_AVX=1 ..\..\..
+if %errorlevel% neq 0 exit /b !errorlevel!
 msbuild /property:Configuration="%1" playground_plug.sln
 if %errorlevel% neq 0 exit /b !errorlevel!
 
 exit /b 0
-
 :usage
 echo "Usage: build_windows Debug|Release"
 exit /b 1
