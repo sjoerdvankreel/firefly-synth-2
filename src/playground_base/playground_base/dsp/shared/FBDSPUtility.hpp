174c9d2916613609b6bc656c3a33fec13cc59e15
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-22 22:13:11 +0100

    651

--- src/playground_base/playground_base/dsp/shared/FBDSPUtility.hpp
+++ src/playground_base/playground_base/dsp/shared/FBDSPUtility.hpp
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <playground_base/base/shared/FBVector.hpp>
+
 #include <cmath>
 #include <numbers>
 
 inline constexpr float FBPi = std::numbers::pi_v<float>;
 inline constexpr float FBTwoPi = 2.0f * FBPi;
 
-inline float 
-FBPitchToFreq(float pitch)
-{ return 440.0f * std::pow(2.0f, (pitch - 69.0f) / 12.0f); } // TODO vector
+inline FBFloatVector
+FBPitchToFreq(FBFloatVector pitch)
+{ return 440.0f * FBFloatVectorPow(2.0f, (pitch - 69.0f) / 12.0f); }
