c48f140421e37e0f64199dbcc1bcdacbe0b47382
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-22 21:39:04 +0100

    649

--- src/playground_base/playground_base/dsp/shared/FBDSPUtility.hpp
+++ src/playground_base/playground_base/dsp/shared/FBDSPUtility.hpp
@@ -8,4 +8,4 @@ inline constexpr float FBTwoPi = 2.0f * FBPi;
 
 inline float 
 FBPitchToFreq(float pitch)
-{ return 440.0f * std::pow(2.0f, (pitch - 69.0f) / 12.0f); }
+{ return 440.0f * std::pow(2.0f, (pitch - 69.0f) / 12.0f); } // TODO vector
