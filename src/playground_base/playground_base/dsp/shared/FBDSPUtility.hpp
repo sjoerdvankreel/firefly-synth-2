30c941f84c6ec1d9eccc2ee5ce9653ad2167b4e6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-23 19:35:14 +0100

    TEMP seems ok

--- src/playground_base/playground_base/dsp/shared/FBDSPUtility.hpp
+++ src/playground_base/playground_base/dsp/shared/FBDSPUtility.hpp
@@ -8,6 +8,10 @@
 inline constexpr float FBPi = std::numbers::pi_v<float>;
 inline constexpr float FBTwoPi = 2.0f * FBPi;
 
+inline float 
+FBPitchToFreq(float pitch)
+{ return 440.0f * std::pow(2.0f, (pitch - 69.0f) / 12.0f); } // TODO vector
+
 inline FBFloatVector
 FBPitchToFreq(FBFloatVector pitch)
 { return 440.0f * FBFloatVectorPow(2.0f, (pitch - 69.0f) / 12.0f); }
--- src/playground_plug/playground_plug/modules/osci/FFOsciProcessor.cpp
+++ src/playground_plug/playground_plug/modules/osci/FFOsciProcessor.cpp
@@ -13,9 +13,33 @@ GenerateSin(FBFloatVector phase)
   return (phase * FBTwoPi).Sin();
 }
 
+// https://www.kvraudio.com/forum/viewtopic.php?t=375517
+static inline float
+generate_blep(float phase, float inc)
+{
+  float b;
+  if (phase < inc) return b = phase / inc, (2.0f - b) * b - 1.0f;
+  if (phase >= 1.0f - inc) return b = (phase - 1.0f) / inc, (b + 2.0f) * b + 1.0f;
+  return 0.0f;
+}
+
+static inline float
+generate_saw(float phase, float inc)
+{
+  float saw = phase * 2 - 1;
+  return saw - generate_blep(phase, inc);
+}
+
 static FBFloatVector
 GenerateSaw(FBFloatVector phase, FBFloatVector incr)
 { 
+  FBFloatVector y;
+  for (int i = 0; i < FBVectorFloatCount; i++)
+  {
+    y[i] = generate_saw(phase[i], incr[i]);
+  }
+  return y;
+  
   // TODO the fast version
   // https://github.com/surge-synthesizer/clap-saw-demo
   // float phaseSteps[3];
@@ -26,6 +50,7 @@ GenerateSaw(FBFloatVector phase, FBFloatVector incr)
   //   phaseSteps[q + 2] = (ph * ph - 1.0f) * ph / 6.0f; }
   // float scale = 0.25f * (1.0f / incr) * (1.0f / incr);
   // y = scale * (phaseSteps[0] + phaseSteps[2] - 2.0f * phaseSteps[1]);
+#if 0
   FBFloatVector result;
   FBFloatVector phaseSteps[3];
   FBFloatVector incrReciprocal = incr.Reciprocal();
@@ -37,12 +62,14 @@ GenerateSaw(FBFloatVector phase, FBFloatVector incr)
   }
   FBFloatVector scale = 0.25f * incrReciprocal * incrReciprocal;
   return scale * (phaseSteps[0] + phaseSteps[2] - 2.0f * phaseSteps[1]);
+#endif
 }
 
 void
 FFOsciProcessor::Process(FFModuleProcState const& state, int voice)
 {
-  float key = static_cast<float>(state.voice->event.note.key);
+  int key = state.voice->event.note.key;
+  float freq = FBPitchToFreq(static_cast<float>(key));
   auto const& glfo = state.proc->dsp.global.glfo[0].output;
   auto& output = state.proc->dsp.voice[voice].osci[state.moduleSlot].output;
 
@@ -61,20 +88,15 @@ FFOsciProcessor::Process(FFModuleProcState const& state, int voice)
     return;
   }
 
-  FBFixedVectorBlock freq;
-  FBFixedVectorBlock incr;
-  FBFixedVectorBlock pitch;
-  pitch.Transform([&](int v) { return key + note - 60.0f + cent[v]; });
-  freq.Transform([&](int v) { return FBPitchToFreq(pitch[v]); });
-  incr.Transform([&](int v) { return freq[v] / state.sampleRate; });
+  float incr = freq / state.sampleRate; // TODO this should be dynamic
   for (int s = 0; s < FBFixedBlockSamples; s++)
-    output.Samples(s, _phase.Next(state.sampleRate, freq.Sample(s)));
+    output.Samples(s, _phase.Next(state.sampleRate, freq));
   switch (type)
   {
   case FFOsciTypeSine: output.Transform([&](int ch, int v) { 
     return GenerateSin(output[ch][v]); }); break;
   case FFOsciTypeSaw: output.Transform([&](int ch, int v) {
-    return GenerateSaw(output[ch][v], incr[v]); }); break;
+    return GenerateSaw(output[ch][v], FBFloatVector(incr)); }); break;
   default: assert(false); break;
   }
   output.Transform([&](int ch, int v) { 
