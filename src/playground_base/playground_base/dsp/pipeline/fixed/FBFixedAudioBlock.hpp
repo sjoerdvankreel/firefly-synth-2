dcb739e6150ff33caba7b45f95a3ac7a4470d3fb
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-24 18:13:34 +0100

    663

--- src/playground_base/playground_base/dsp/pipeline/fixed/FBFixedAudioBlock.hpp
+++ src/playground_base/playground_base/dsp/pipeline/fixed/FBFixedAudioBlock.hpp
@@ -18,16 +18,17 @@ public:
   template <class Op>
   void Transform(Op op);
 
+  void Clear();
+  void Add(FBFixedAudioBlock const& rhs);
+  void CopyFrom(FBFixedAudioBlock const& rhs);
+  void CopyFrom(FBBufferAudioBlock const& rhs);
+
   FBFixedVectorBlock& operator[](int ch) { return _store[ch]; }
   FBFixedVectorBlock const& operator[](int ch) const { return _store[ch]; }
+
   float Sample(int ch, int index) const { return _store[ch].Sample(index); }
   void Sample(int ch, int index, float val) { _store[ch].Sample(index, val); }
   void Samples(int index, float val) { _store[0].Sample(index, val); _store[1].Sample(index, val); }
-
-  void CopyFrom(FBFixedAudioBlock const& rhs);
-  void CopyFrom(FBBufferAudioBlock const& rhs);
-  void Clear() { for (int ch = 0; ch < 2; ch++) _store[ch].Clear(); }
-  void Add(FBFixedAudioBlock const& rhs) { for (int ch = 0; ch < 2; ch++) _store[ch].Add(rhs._store[ch]); }
 };
 
 template <class Op>
@@ -39,10 +40,32 @@ FBFixedAudioBlock::Transform(Op op)
       (*this)[ch][v] = op(ch, v);
 }
 
+inline void 
+FBFixedAudioBlock::Clear() 
+{ 
+  for (int ch = 0; ch < 2; ch++) 
+    _store[ch].Clear();
+}
+
+inline void 
+FBFixedAudioBlock::Add(FBFixedAudioBlock const& rhs) 
+{ 
+  for (int ch = 0; ch < 2; ch++) 
+    _store[ch].Add(rhs._store[ch]); 
+}
+
 inline void
 FBFixedAudioBlock::CopyFrom(FBFixedAudioBlock const& rhs)
 {
   for (int ch = 0; ch < 2; ch++)
     for (int v = 0; v < FBFixedBlockVectors; v++)
       (*this)[ch][v] = rhs[ch][v];
+}
+
+inline void
+FBFixedAudioBlock::CopyFrom(FBBufferAudioBlock const& rhs)
+{
+  for (int ch = 0; ch < 2; ch++)
+    for (int s = 0; s < FBFixedBlockSamples; s++)
+      Sample(ch, s, rhs[ch][s]);
 }
