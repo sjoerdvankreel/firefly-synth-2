7863934fedc288537c0b4a7e46e22cf8af0987c0
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-26 22:00:07 +0100

    685

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -11,3 +11,9 @@ mkdir -p "build/linux/$1/SSE"
 cd "build/linux/$1/SSE"
 cmake -DCMAKE_BUILD_TYPE="$1" -DFB_USE_SSE=1 -DFB_USE_AVX=0 ../../../..
 make
+
+cd ../../../..
+mkdir -p "build/linux/$1/AVX"
+cd "build/linux/$1/AVX"
+cmake -DCMAKE_BUILD_TYPE="$1" -DFB_USE_SSE=0 -DFB_USE_AVX=1 ../../../..
+make
