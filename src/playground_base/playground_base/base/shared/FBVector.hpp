bd22c6b14d1743ac8e1f92b008bda81dc315b15d
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-12-29 21:45:40 +0100

    743

--- src/playground_base/playground_base/base/shared/FBVector.hpp
+++ src/playground_base/playground_base/base/shared/FBVector.hpp
@@ -2,20 +2,26 @@
 
 #include <xsimd/xsimd.hpp>
 
-#define FB_ARCH_TYPE_SSE2 1
-#define FB_ARCH_TYPE_AVX2 2
-#define FB_ARCH_TYPE_AARCH64 3
+#define FB_ARCH_TYPE_WIN_LIN_SSE2 1
+#define FB_ARCH_TYPE_WIN_LIN_AVX2 2
+#define FB_ARCH_TYPE_MAC_UNIVERSAL 3
 
-#ifdef FB_ARCH_TYPE
-#if FB_ARCH_TYPE == FB_ARCH_TYPE_SSE2
-typedef xsimd::batch<float, xsimd::sse2> FBFloatVector;
-#elif FB_ARCH_TYPE == FB_ARCH_TYPE_AVX2
-typedef xsimd::batch<float, xsimd::avx2> FBFloatVector;
-#elif FB_ARCH_TYPE == FB_ARCH_TYPE_AARCH64
-typedef xsimd::batch<float, xsimd::neon64> FBFloatVector;
+#ifndef FB_ARCH_TYPE
+#error
 #endif
+
+#if FB_ARCH_TYPE == FB_ARCH_TYPE_WIN_LIN_SSE2
+typedef xsimd::batch<float, xsimd::sse2> FBFloatVector;
+#elif FB_ARCH_TYPE == FB_ARCH_TYPE_WIN_LIN_AVX2
+typedef xsimd::batch<float, xsimd::avx2> FBFloatVector;
+#elif FB_ARCH_TYPE == FB_ARCH_TYPE_MAC_UNIVERSAL
+#ifdef __x86_64__
+typedef xsimd::batch<float, xsimd::sse2> FBFloatVector;
+#elif defined(__aarch64__)
+typedef xsimd::batch<float, xsimd::neon64> FBFloatVector;
 #else
 #error
+#endif
 #endif
 
 inline int constexpr FBVectorFloatCount = sizeof(FBFloatVector) / sizeof(float);
