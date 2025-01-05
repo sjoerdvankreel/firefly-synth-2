a027dd41be891b075c9cfbfb189779d7ccf3bc40
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-01-05 16:39:46 +0100

    833

--- src/playground_plug/playground_plug/modules/gfilter/FFGFilterProcessor.cpp
+++ src/playground_plug/playground_plug/modules/gfilter/FFGFilterProcessor.cpp
@@ -15,7 +15,7 @@ FFGFilterProcessor::Process(FFModuleProcState const& state)
   auto const& input = state.proc->dsp.global.gFilter[state.moduleSlot].input;
   auto const& params = state.proc->param.global.gFilter[state.moduleSlot];
   bool on = topo.params[(int)FFGFilterParam::On].boolean.NormalizedToPlain(params.block.on[0].Value());
-  // todo type
+  auto type = (FFGFilterType)topo.params[(int)FFGFilterParam::Type].list.NormalizedToPlain(params.block.type[0].Value());
 
   if (!on)
   {
@@ -23,13 +23,38 @@ FFGFilterProcessor::Process(FFModuleProcState const& state)
     return;
   }
 
-  FBFixedDoubleBlock res, freq, g, k;
+  FBFixedDoubleBlock res, freq, gain;
   res.LoadCastFromFloatArray(params.acc.res[0].Global().CV());
   freq.LoadCastFromFloatArray(params.acc.freq[0].Global().CV());
+  
+  FBFixedDoubleBlock a;
+  switch (type)
+  {
+  case FFGFilterType::BLL:
+  case FFGFilterType::LSH:
+  case FFGFilterType::HSH:
+    gain.LoadCastFromFloatArray(params.acc.gain[0].Global().CV());
+    a.Transform([&](int v) {
+      auto plainGain = topo.params[(int)FFGFilterParam::Gain].linear.NormalizedToPlain(gain[v]);
+      return xsimd::pow(FBDoubleVector(10.0), plainGain / 40.0); });
+    break;
+  default:
+    break;
+  }
+
+  FBFixedDoubleBlock g, k;
   k.Transform([&](int v) { return 2.0 - 2.0 * res[v]; });
   g.Transform([&](int v) {
     auto plainFreq = topo.params[(int)FFGFilterParam::Freq].freqOct.NormalizedToPlain(freq[v]);
     return xsimd::tan(std::numbers::pi * plainFreq / state.sampleRate); });
+
+  switch (type)
+  {
+  case FFGFilterType::BLL: k.Transform([&](int v) { return k[v] / a[v]; }); break;
+  case FFGFilterType::LSH: g.Transform([&](int v) { return g[v] / xsimd::sqrt(a[v]); }); break;
+  case FFGFilterType::HSH: g.Transform([&](int v) { return g[v] * xsimd::sqrt(a[v]); }); break;
+  default: break;
+  }
   
   FBFixedDoubleBlock a1b, a2b, a3b;
   a1b.Transform([&](int v) { return 1.0 / (1.0 + g[v] * (g[v] + k[v])); });
@@ -40,6 +65,61 @@ FFGFilterProcessor::Process(FFModuleProcState const& state)
   a1b.StoreToDoubleArray(a1a);
   a2b.StoreToDoubleArray(a2a);
   a3b.StoreToDoubleArray(a3a);
+
+  FBFixedDoubleBlock m0b, m1b, m2b;
+  switch (type)
+  {
+  case FFGFilterType::LPF:
+    m0b.Transform([&](int v) { return 0.0; });
+    m1b.Transform([&](int v) { return 0.0; });
+    m2b.Transform([&](int v) { return 1.0; });
+    break;
+  case FFGFilterType::BPF:
+    m0b.Transform([&](int v) { return 0.0; });
+    m1b.Transform([&](int v) { return 1.0; });
+    m2b.Transform([&](int v) { return 0.0; });
+    break;
+  case FFGFilterType::HPF:
+    m0b.Transform([&](int v) { return 1.0; });
+    m1b.Transform([&](int v) { return -k[v]; });
+    m2b.Transform([&](int v) { return -1.0; });
+    break;
+  case FFGFilterType::BSF:
+    m0b.Transform([&](int v) { return 1.0; });
+    m1b.Transform([&](int v) { return -k[v]; });
+    m2b.Transform([&](int v) { return 0.0; });
+    break;
+  case FFGFilterType::PEQ:
+    m0b.Transform([&](int v) { return 1.0; });
+    m1b.Transform([&](int v) { return -k[v]; });
+    m2b.Transform([&](int v) { return -2.0; });
+    break;
+  case FFGFilterType::APF:
+    m0b.Transform([&](int v) { return 1.0; });
+    m1b.Transform([&](int v) { return -2.0 * k[v]; });
+    m2b.Transform([&](int v) { return 0.0; });
+    break;
+  case FFGFilterType::BLL:
+    m0b.Transform([&](int v) { return 1.0; });
+    m1b.Transform([&](int v) { return k[v] * (a[v] * a[v] - 1.0); });
+    m2b.Transform([&](int v) { return 0.0; });
+    break;
+  case FFGFilterType::LSH:
+    m0b.Transform([&](int v) { return 1.0; });
+    m1b.Transform([&](int v) { return k[v] * (a[v] - 1.0); });
+    m2b.Transform([&](int v) { return a[v] * a[v] - 1.0; });
+    break;
+  case FFGFilterType::HSH:
+    m0b.Transform([&](int v) { return a[v] * a[v]; });
+    m1b.Transform([&](int v) { return k[v] * (1.0 - a[v]) * a[v]; });
+    m2b.Transform([&](int v) { return 1.0 - a[v] * a[v]; });
+    break;
+  }
+
+  FBFixedDoubleArray m0a, m1a, m2a;
+  m0b.StoreToDoubleArray(m0a);
+  m1b.StoreToDoubleArray(m1a);
+  m2b.StoreToDoubleArray(m2a);
 
   FBFixedDoubleAudioArray audioIn;
   FBFixedDoubleAudioArray audioOut = {};
@@ -54,7 +134,7 @@ FFGFilterProcessor::Process(FFModuleProcState const& state)
       double v2 = _ic2eq[ch] + a2a.data[s] * _ic1eq[ch] + a3a.data[s] * v3;
       _ic1eq[ch] = 2 * v1 - _ic1eq[ch];
       _ic2eq[ch] = 2 * v2 - _ic2eq[ch];
-      audioOut.data[ch].data[s] = v2;
+      audioOut.data[ch].data[s] = m0a.data[s] * v0 + m1a.data[s] * v1 + m2a.data[s] * v2;
     }
   output.LoadCastFromDoubleArray(audioOut);  
 }
