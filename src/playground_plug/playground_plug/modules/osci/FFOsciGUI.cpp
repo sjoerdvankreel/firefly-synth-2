fb82f84532076d586e8b03bd2d1fdf7211d4c4e1
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-01-21 23:56:54 +0100

    Revert "1136"
    This reverts commit 30d84771d79378473b5e01ab0c5a1a30c4e9e0d7.

--- src/playground_plug/playground_plug/modules/osci/FFOsciGUI.cpp
+++ src/playground_plug/playground_plug/modules/osci/FFOsciGUI.cpp
@@ -57,8 +57,12 @@ MakeSectionGain(FBPlugGUI* plugGUI, int moduleSlot)
   auto gLFOToGain = plugGUI->Topo()->ParamAtTopo({ (int)FFModuleType::Osci, moduleSlot, (int)FFOsciParam::GLFOToGain, 0 });
   grid->Add(0, 4, plugGUI->AddComponent<FBParamLabel>(plugGUI, gLFOToGain));
   grid->Add(0, 5, plugGUI->AddComponent<FBParamSlider>(plugGUI, gLFOToGain, Slider::SliderStyle::Rotary));
-  FBTopoIndices indices = { (int)FFModuleType::Osci, moduleSlot };
-  FBParamsDependency dependency = { { (int)FFOsciParam::On }, [](auto const& vs) { return vs[0] != 0; } };
+  // TODO less ugly
+  FBTopoIndices indices;
+  indices.index = (int)FFModuleType::Osci;
+  indices.slot = moduleSlot;
+  FBParamsDependency dependency;
+  dependency.When({ (int)FFOsciParam::On }, [](auto const& vs) { return vs[0] != 0; });
   return plugGUI->AddComponent<FBParamsDependentSectionComponent>(plugGUI, grid, indices, dependency);
 }
 
