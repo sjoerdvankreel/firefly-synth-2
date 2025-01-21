b911de8c457c9463d41144893fbd47c9392ceed1
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-01-21 17:59:22 +0100

    1110

--- src/playground_plug/playground_plug/modules/gfilter/FFGFilterGUI.hpp
+++ src/playground_plug/playground_plug/modules/gfilter/FFGFilterGUI.hpp
@@ -2,12 +2,7 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
-struct FBRuntimeTopo;
 class FBPlugGUI;
-class IFBHostGUIContext;
 
 juce::Component*
-FFMakeGFilterGUI(
-  FBRuntimeTopo const* topo, 
-  FBPlugGUI* plugGUI,
-  IFBHostGUIContext* hostContext);
+FFMakeGFilterGUI(FBPlugGUI* plugGUI);
