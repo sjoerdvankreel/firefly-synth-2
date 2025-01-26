9801a7f32217f6d8720e8530dad9d887f9b8f8ae
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-01-26 18:26:12 +0100

    1182

--- src/playground_plug/playground_plug/modules/env/FFEnvProcessor.cpp
+++ src/playground_plug/playground_plug/modules/env/FFEnvProcessor.cpp
@@ -12,25 +12,23 @@
 void
 FFEnvProcessor::BeginVoice(FFModuleProcState const& state)
 {
-  // TODO
   _position = 0;
   _stage = FFEnvStage::Delay;
+  int voice = state.voice->slot;
+  auto const& topo = state.topo->modules[(int)FFModuleType::Env];
+  auto const& params = state.proc->param.voice.env[state.moduleSlot];
+  _voiceState.on = topo.params[(int)FFEnvParam::On].boolean.NormalizedToPlain(params.block.on[0].Voice()[voice]);
+  _voiceState.type = (FFEnvType)topo.params[(int)FFEnvParam::Type].list.NormalizedToPlain(params.block.type[0].Voice()[voice]);
+  _voiceState.holdSamples = (int)std::round(topo.params[(int)FFEnvParam::HoldTime].linear.NormalizedToPlain(params.block.holdTime[0].Voice()[voice]) * state.sampleRate);
+  _voiceState.delaySamples = (int)std::round(topo.params[(int)FFEnvParam::DelayTime].linear.NormalizedToPlain(params.block.delayTime[0].Voice()[voice]) * state.sampleRate);
+  _voiceState.decaySamples = (int)std::round(topo.params[(int)FFEnvParam::DecayTime].linear.NormalizedToPlain(params.block.decayTime[0].Voice()[voice]) * state.sampleRate);
+  _voiceState.attackSamples = (int)std::round(topo.params[(int)FFEnvParam::AttackTime].linear.NormalizedToPlain(params.block.attackTime[0].Voice()[voice]) * state.sampleRate);
+  _voiceState.releaseSamples = (int)std::round(topo.params[(int)FFEnvParam::ReleaseTime].linear.NormalizedToPlain(params.block.releaseTime[0].Voice()[voice]) * state.sampleRate);
 }
 
 void 
 FFEnvProcessor::Process(FFModuleProcState const& state)
 {
-  int voice = state.voice->slot;
-  auto const& topo = state.topo->modules[(int)FFModuleType::Env];
-  auto const& params = state.proc->param.voice.env[state.moduleSlot];
-  bool on = topo.params[(int)FFEnvParam::On].boolean.NormalizedToPlain(params.block.on[0].Voice()[voice]);
-  int type = topo.params[(int)FFEnvParam::Type].list.NormalizedToPlain(params.block.type[0].Voice()[voice]);
-  int holdSamples = (int)std::round(topo.params[(int)FFEnvParam::HoldTime].linear.NormalizedToPlain(params.block.holdTime[0].Voice()[voice]) * state.sampleRate);
-  int delaySamples = (int)std::round(topo.params[(int)FFEnvParam::DelayTime].linear.NormalizedToPlain(params.block.delayTime[0].Voice()[voice]) * state.sampleRate);
-  int decaySamples = (int)std::round(topo.params[(int)FFEnvParam::DecayTime].linear.NormalizedToPlain(params.block.decayTime[0].Voice()[voice]) * state.sampleRate);
-  int attackSamples = (int)std::round(topo.params[(int)FFEnvParam::AttackTime].linear.NormalizedToPlain(params.block.attackTime[0].Voice()[voice]) * state.sampleRate);
-  int releaseSamples = (int)std::round(topo.params[(int)FFEnvParam::ReleaseTime].linear.NormalizedToPlain(params.block.releaseTime[0].Voice()[voice]) * state.sampleRate);
-
   FBFixedFloatArray scratch = {};
   for (int s = 0; s < FBFixedBlockSamples; s++)
   {
--- src/playground_plug/playground_plug/modules/env/FFEnvProcessor.hpp
+++ src/playground_plug/playground_plug/modules/env/FFEnvProcessor.hpp
@@ -1,13 +1,26 @@
 #pragma once
 
+#include <playground_plug/modules/env/FFEnvTopo.hpp>
 #include <playground_base/base/shared/FBLifetime.hpp>
 
 struct FFModuleProcState;
 enum class FFEnvStage { Delay, Attack, Hold, Decay, Sustain, Release };
 
+struct FFEnvVoiceState
+{
+  bool on = {};
+  FFEnvType type = {};
+  int holdSamples = {};
+  int delaySamples = {};
+  int decaySamples = {};
+  int attackSamples = {};
+  int releaseSamples = {};
+};
+
 class FFEnvProcessor final
 {
   int _position = 0;
+  FFEnvVoiceState _voiceState = {};
   FFEnvStage _stage = FFEnvStage::Delay;
 
 public:
