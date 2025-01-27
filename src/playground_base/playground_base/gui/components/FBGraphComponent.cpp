f69668987595c8b94fbdddc4ce5d388944c8731b
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-01-27 19:47:11 +0100

    1209

--- src/playground_base/playground_base/gui/components/FBGraphComponent.cpp
+++ src/playground_base/playground_base/gui/components/FBGraphComponent.cpp
@@ -1,9 +1,23 @@
 #include <playground_base/gui/components/FBGraphComponent.hpp>
 
+#include <cmath>
+#include <numbers>
+
 using namespace juce;
 
 void
 FBGraphComponent::paint(Graphics& g)
 {
-  g.fillAll(Colours::red);
+  // TODO
+  Path p;
+  g.fillAll(Colours::black);
+  p.startNewSubPath(0.0, 0.0);
+  for (int i = 0; i < getWidth(); i++)
+  {
+    float phase = (float)i / (getWidth() + 1);
+    float sine = (std::sin(phase * 2.0f * std::numbers::pi_v<float>) + 1.0f) * 0.5f;
+    p.lineTo((float)i, sine * getHeight());
+  }      
+  g.setColour(Colours::white);
+  g.strokePath(p, PathStrokeType(1.0f));
 }
