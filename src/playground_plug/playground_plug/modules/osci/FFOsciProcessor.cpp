03bcf8bb2c3c631d4839974a38d52fa6563def41
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-02-05 18:51:07 +0100

    1339

--- src/playground_plug/playground_plug/modules/osci/FFOsciProcessor.cpp
+++ src/playground_plug/playground_plug/modules/osci/FFOsciProcessor.cpp
@@ -105,10 +105,17 @@ FFOsciProcessor::Process(FBModuleProcState const& state)
     return GeneratePulse(phase[v], incr[v], pw.CV(v)); }); break;
   default: assert(false); break;
   }
-  mono.Transform([&](int v) { 
+
+  // TODO not ideal -- only need 1 sample
+  FBFixedFloatBlock gain1WithGLFOBlock;
+  FBFixedFloatArray gain1WithGLFOArray;
+  gain1WithGLFOBlock.Transform([&](int v) {
     auto gLFOToGainBlock = gLFOToGain.CV(v);
-    auto modByGLFO = (1.0f - gLFOToGainBlock) * mono[v] + gLFOToGainBlock * gLFO[v] * mono[v];
-    return gain1.CV(v) * gain2.CV(v) * modByGLFO; });
+    return (1.0f - gLFOToGainBlock) * gain1.CV(v) + gLFOToGainBlock * gLFO[v] * gain1.CV(v); });
+  gain1WithGLFOBlock.StoreToFloatArray(gain1WithGLFOArray);
+
+  mono.Transform([&](int v) {
+    return gain1WithGLFOBlock[v] * gain2.CV(v) * mono[v]; });
   output.Transform([&](int ch, int v) { return mono[v]; });
 
   auto* exchangeState = state.ExchangeState<FFExchangeState>();
@@ -118,7 +125,7 @@ FFOsciProcessor::Process(FBModuleProcState const& state)
   auto& exchangeParams = exchangeState->param.voice.osci[state.moduleSlot];
   exchangeParams.acc.pw[0][voice] = pw.CV().data[FBFixedBlockSamples - 1];
   exchangeParams.acc.cent[0][voice] = cent.CV().data[FBFixedBlockSamples - 1];
-  exchangeParams.acc.gain[0][voice] = gain1.CV().data[FBFixedBlockSamples - 1];
   exchangeParams.acc.gain[1][voice] = gain2.CV().data[FBFixedBlockSamples - 1];
+  exchangeParams.acc.gain[0][voice] = gain1WithGLFOArray.data[FBFixedBlockSamples - 1];
   exchangeParams.acc.gLFOToGain[0][voice] = gLFOToGain.CV().data[FBFixedBlockSamples - 1];
 }
