e76c8215d916a4d454fcea7ff1c0c771a9517fc8
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-02-08 16:52:55 +0100

    1374

--- src/playground_plug/playground_plug/shared/FFPlugTopo.cpp
+++ src/playground_plug/playground_plug/shared/FFPlugTopo.cpp
@@ -10,6 +10,20 @@
 #include <playground_base/base/topo/static/FBStaticTopo.hpp>
 #include <playground_base/base/topo/static/FBStaticModule.hpp>
 
+static FBStaticTopoGUI
+MakeTopoGUI()
+{
+  FBStaticTopoGUI result = {};
+  result.plugWidth = 800;
+  result.minUserScale = 0.5f;
+  result.maxUserScale = 16.0f;
+  result.aspectRatioWidth = 2;
+  result.aspectRatioHeight = 1;
+  result.factory = [](FBHostGUIContext* hostContext) {
+    return std::make_unique<FFPlugGUI>(hostContext); };
+  return result;
+}
+
 static FBSpecialParam
 MakeSpecialParam(
   FBStaticTopo const& topo, void* state, 
@@ -23,38 +37,45 @@ MakeSpecialParam(
   return result;
 }
 
+static FBSpecialParams
+SpecialParamsSelector(
+  FBStaticTopo const& topo, void* state)
+{
+  FBSpecialParams result = {};
+  result.hostSmoothTime = MakeSpecialParam(
+    topo, state, (int)FFModuleType::Master, (int)FFMasterParam::HostSmoothTime);
+  return result;
+}
+
+static FBStaticTopoState
+MakeTopoState()
+{
+  FBStaticTopoState result = {};
+  result.specialSelector = SpecialParamsSelector;
+  result.exchangeStateSize = sizeof(FFExchangeState);
+  result.exchangeStateAlignment = alignof(FFExchangeState);
+  result.allocRawProcState = []() { return static_cast<void*>(new FFProcState); };
+  result.allocRawScalarState = []() { return static_cast<void*>(new FFScalarState); };
+  result.allocRawExchangeState = []() { return static_cast<void*>(new FFExchangeState); };
+  result.freeRawProcState = [](void* state) { delete static_cast<FFProcState*>(state); };
+  result.freeRawScalarState = [](void* state) { delete static_cast<FFScalarState*>(state); };
+  result.freeRawExchangeState = [](void* state) { delete static_cast<FFExchangeState*>(state); };
+  result.voiceStateExchangeAddr = [](void* state) { return &static_cast<FFExchangeState*>(state)->voiceState; };
+  return result;
+}
+
 std::unique_ptr<FBStaticTopo>
 FFMakeTopo()
 {
   auto result = std::make_unique<FBStaticTopo>();
   result->meta = FFPlugMeta();
+  result->gui = MakeTopoGUI();
+  result->state = MakeTopoState();
   result->modules.resize((int)FFModuleType::Count);
   result->modules[(int)FFModuleType::Env] = std::move(*FFMakeEnvTopo());
   result->modules[(int)FFModuleType::GLFO] = std::move(*FFMakeGLFOTopo());
   result->modules[(int)FFModuleType::Osci] = std::move(*FFMakeOsciTopo());
   result->modules[(int)FFModuleType::Master] = std::move(*FFMakeMasterTopo());
   result->modules[(int)FFModuleType::GFilter] = std::move(*FFMakeGFilterTopo());
-
-  result->gui.plugWidth = 800;
-  result->gui.minUserScale = 0.5f;
-  result->gui.maxUserScale = 16.0f;
-  result->gui.aspectRatioWidth = 2;
-  result->gui.aspectRatioHeight = 1;
-  result->gui.factory = [](FBHostGUIContext* hostContext) { return std::make_unique<FFPlugGUI>(hostContext); };
-
-  result->state.allocRawProcState = []() { return static_cast<void*>(new FFProcState); };
-  result->state.allocRawScalarState = []() { return static_cast<void*>(new FFScalarState); };
-  result->state.allocRawExchangeState = []() { return static_cast<void*>(new FFExchangeState); };
-  result->state.freeRawProcState = [](void* state) { delete static_cast<FFProcState*>(state); };
-  result->state.freeRawScalarState = [](void* state) { delete static_cast<FFScalarState*>(state); };
-  result->state.freeRawExchangeState = [](void* state) { delete static_cast<FFExchangeState*>(state); };
-
-  result->state.exchangeStateSize = sizeof(FFExchangeState);
-  result->state.exchangeStateAlignment = alignof(FFExchangeState);
-  result->state.voiceStateExchangeAddr = [](void* state) { return &static_cast<FFExchangeState*>(state)->voiceState; };
-  result->state.specialSelector = [](FBStaticTopo const& topo, void* state) {
-    FBSpecialParams params = {};
-    params.hostSmoothTime = MakeSpecialParam(topo, state, (int)FFModuleType::Master, (int)FFMasterParam::HostSmoothTime);
-    return params; };
   return result;
 }
