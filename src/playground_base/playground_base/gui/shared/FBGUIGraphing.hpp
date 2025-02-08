6476265dce6ca9c2e33c6062b67367739251af99
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-02-08 21:53:03 +0100

    1386

--- src/playground_base/playground_base/gui/shared/FBGUIGraphing.hpp
+++ src/playground_base/playground_base/gui/shared/FBGUIGraphing.hpp
@@ -15,9 +15,13 @@ FBModuleGraphGlobalOutputSelector;
 typedef std::function<FBFixedFloatBlock const* (
   void const* procState, int voice, int moduleSlot)>
 FBModuleGraphVoiceOutputSelector;
+
 typedef std::function<FBModuleProcExchangeState const* (
-  void const* rawExchangeState)>
-FBModuleGraphExchangeStateSelector;    
+  void const* exchangeState, int moduleSlot)>
+FBModuleGraphGlobalExchangeSelector;    
+typedef std::function<FBModuleProcExchangeState const* (
+  void const* exchangeState, int voice, int moduleSlot)>
+FBModuleGraphVoiceExchangeSelector;    
 
 template <class Processor>
 struct FBModuleGraphRenderData final
@@ -27,7 +31,8 @@ struct FBModuleGraphRenderData final
   FBModuleGraphComponentData* graphData = {};
   FBModuleGraphVoiceOutputSelector voiceOutputSelector = {};
   FBModuleGraphGlobalOutputSelector globalOutputSelector = {};
-  FBModuleGraphExchangeStateSelector exchangeStateSelector = {};
+  FBModuleGraphVoiceExchangeSelector voiceExchangeSelector = {};
+  FBModuleGraphGlobalExchangeSelector globalExchangeSelector = {};
 };
 
 template <bool Global, class Processor> 
@@ -64,43 +69,44 @@ FBRenderModuleGraphSeries(
   }
 }
 
-template <
-  bool Global,
-  class Processor, 
-  class RenderData = FBModuleGraphRenderData<Processor>>
+template <bool Global, class RenderData>
 void
-FBRenderModuleGraph(RenderData const& renderData)
+FBRenderModuleGraph(RenderData& renderData)
 {
   assert(renderData.moduleType != -1);
   assert(renderData.graphData != nullptr);
-  assert(renderData.exchangeStateSelector != nullptr);
   assert((renderData.voiceOutputSelector == nullptr) != (renderData.globalOutputSelector == nullptr));
+  assert((renderData.voiceExchangeSelector == nullptr) != (renderData.globalExchangeSelector == nullptr));
 
   auto graphData = renderData.graphData;
   auto renderState = graphData->renderState;
   auto moduleProcState = renderState->ModuleProcState();
   auto scalarState = renderState->ScalarContainer()->Raw();
   auto exchangeState = renderState->ExchangeContainer()->Raw();
-  auto const& moduleExchange = moduleExchangeSelector(exchangeState);
+
+  // TODO voice
+  FBModuleProcExchangeState const* moduleExchange = nullptr;
+  if constexpr (Global)
+    moduleExchange = renderData.globalExchangeSelector(exchangeState, moduleProcState->moduleSlot);
 
   // TODO per voice
   graphData->text = "OFF";
-  if (!moduleExchange.active)
+  if (!moduleExchange->active)
     return;
 
   float guiSampleCount = (float)graphData->pixelWidth;
-  float dspSampleCount = (float)moduleExchange.lengthSamples;
+  float dspSampleCount = (float)moduleExchange->lengthSamples;
   float dspSampleRate = renderState->ExchangeContainer()->SampleRate();
   moduleProcState->sampleRate = dspSampleRate / (dspSampleCount / guiSampleCount);  
 
   renderState->PrepareForRenderPrimary();
-  FBRenderModuleGraphSeries(renderData, graphData->primarySeries);
+  FBRenderModuleGraphSeries<Global>(renderData, graphData->primarySeries);
   float durationSeconds = renderData.graphData->primarySeries.size() / moduleProcState->sampleRate;
   renderData.graphData->text = FBFormatFloat(durationSeconds, FBDefaultDisplayPrecision) + " Sec";
   
   renderState->PrepareForRenderExchange();  
-  assert(moduleExchange.positionSamples < moduleExchange.lengthSamples);
-  float positionNormalized = moduleExchange.positionSamples / (float)moduleExchange.lengthSamples;
+  assert(moduleExchange->positionSamples < moduleExchange->lengthSamples);
+  float positionNormalized = moduleExchange->positionSamples / (float)moduleExchange->lengthSamples;
 
   // TODO voice
   if (renderState->GlobalModuleExchangeStateEqualsPrimary((int)FFModuleType::GLFO, moduleProcState->moduleSlot))
@@ -111,7 +117,7 @@ FBRenderModuleGraph(RenderData const& renderData)
   }
 
   auto& secondary = graphData->secondarySeries.emplace_back();
-  FBRenderModuleGraphSeries(renderData, secondary.points);
+  FBRenderModuleGraphSeries<Global>(renderData, secondary.points);
   secondary.marker = (int)(positionNormalized * secondary.points.size());
   assert(secondary.marker < secondary.points.size());
 }
--- src/playground_plug/playground_plug/modules/glfo/FFGLFOGraph.cpp
+++ src/playground_plug/playground_plug/modules/glfo/FFGLFOGraph.cpp
@@ -5,7 +5,6 @@
 #include <playground_base/gui/shared/FBGUIGraphing.hpp>
 #include <playground_base/gui/components/FBModuleGraphComponentData.hpp>
 
-#include <playground_base/base/shared/FBFormat.hpp>
 #include <playground_base/base/state/proc/FBModuleProcState.hpp>
 #include <playground_base/base/state/main/FBGraphRenderState.hpp>
 #include <playground_base/base/state/main/FBScalarStateContainer.hpp>
@@ -16,43 +15,12 @@
 void
 FFGLFORenderGraph(FBModuleGraphComponentData* graphData)
 {
-  graphData->text = "OFF";
-  auto renderState = graphData->renderState;
-  auto moduleProcState = renderState->ModuleProcState();
-  auto scalarState = renderState->ScalarContainer()->Raw();
-  auto exchangeState = renderState->ExchangeContainer()->As<FFExchangeState>();
-  auto const& moduleExchange = exchangeState->global.gLFO[moduleProcState->moduleSlot];
-
-  if (!moduleExchange.active)
-    return;
-    
-  FBModuleGraphRenderData<FFGLFOProcessor> renderData;
+  FBModuleGraphRenderData<FFGLFOProcessor> renderData = {};
   renderData.graphData = graphData;
+  renderData.moduleType = (int)FFModuleType::GLFO;
+  renderData.globalExchangeSelector = [](void const* exchangeState, int slot) {
+    return &static_cast<FFExchangeState const*>(exchangeState)->global.gLFO[slot]; };
   renderData.globalOutputSelector = [](void const* procState, int slot) {
     return &static_cast<FFProcState const*>(procState)->dsp.global.gLFO[slot].output; };
-
-  float guiSampleCount = (float)graphData->pixelWidth;
-  float dspSampleCount = (float)moduleExchange.lengthSamples;
-  float dspSampleRate = renderState->ExchangeContainer()->SampleRate();
-  moduleProcState->sampleRate = dspSampleRate / (dspSampleCount / guiSampleCount);  
-
-  renderState->PrepareForRenderPrimary();
-  FBRenderModuleGraphSeries<true>(renderData, graphData->primarySeries);
-  float durationSeconds = renderData.graphData->primarySeries.size() / moduleProcState->sampleRate;
-  renderData.graphData->text = FBFormatFloat(durationSeconds, FBDefaultDisplayPrecision) + " Sec";
-  
-  renderState->PrepareForRenderExchange();  
-  assert(moduleExchange.positionSamples < moduleExchange.lengthSamples);
-  float positionNormalized = moduleExchange.positionSamples / (float)moduleExchange.lengthSamples;
-  if (renderState->GlobalModuleExchangeStateEqualsPrimary((int)FFModuleType::GLFO, moduleProcState->moduleSlot))
-  {
-    graphData->primaryMarkers.push_back((int)(positionNormalized * graphData->primarySeries.size()));
-    assert(graphData->primaryMarkers[graphData->primaryMarkers.size() - 1] < graphData->primarySeries.size());
-    return;
-  }
-
-  auto& secondary = graphData->secondarySeries.emplace_back();
-  FBRenderModuleGraphSeries<true>(renderData, secondary.points);
-  secondary.marker = (int)(positionNormalized * secondary.points.size());
-  assert(secondary.marker < secondary.points.size());
+  FBRenderModuleGraph<true>(renderData);
 }
