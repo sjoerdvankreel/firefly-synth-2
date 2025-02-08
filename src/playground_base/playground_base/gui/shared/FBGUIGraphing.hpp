99d2a217941c79d5445e87f760acff54ffb2e67e
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-02-08 21:39:01 +0100

    1385

--- src/playground_base/playground_base/gui/shared/FBGUIGraphing.hpp
+++ src/playground_base/playground_base/gui/shared/FBGUIGraphing.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <playground_base/base/shared/FBFormat.hpp>
+
 #include <vector>
+#include <cassert>
 #include <functional>
 
 class FBFixedFloatBlock;
@@ -12,14 +15,19 @@ FBModuleGraphGlobalOutputSelector;
 typedef std::function<FBFixedFloatBlock const* (
   void const* procState, int voice, int moduleSlot)>
 FBModuleGraphVoiceOutputSelector;
+typedef std::function<FBModuleProcExchangeState const* (
+  void const* rawExchangeState)>
+FBModuleGraphExchangeStateSelector;    
 
 template <class Processor>
 struct FBModuleGraphRenderData final
 {
+  int moduleType = -1;
   Processor processor = {};
   FBModuleGraphComponentData* graphData = {};
   FBModuleGraphVoiceOutputSelector voiceOutputSelector = {};
   FBModuleGraphGlobalOutputSelector globalOutputSelector = {};
+  FBModuleGraphExchangeStateSelector exchangeStateSelector = {};
 };
 
 template <bool Global, class Processor> 
@@ -35,7 +43,7 @@ FBRenderModuleGraphSeries(
   auto moduleProcState = renderState->ModuleProcState();
   int moduleSlot = moduleProcState->moduleSlot;
 
-  if constexpr (Global)
+  if constexpr(Global)
     renderData.processor.Reset(*moduleProcState);
   else
     renderData.processor.BeginVoice(*moduleProcState);
@@ -54,4 +62,56 @@ FBRenderModuleGraphSeries(
     for (int i = 0; i < processed; i++)
       seriesOut.push_back(seriesIn.data[i]);
   }
+}
+
+template <
+  bool Global,
+  class Processor, 
+  class RenderData = FBModuleGraphRenderData<Processor>>
+void
+FBRenderModuleGraph(RenderData const& renderData)
+{
+  assert(renderData.moduleType != -1);
+  assert(renderData.graphData != nullptr);
+  assert(renderData.exchangeStateSelector != nullptr);
+  assert((renderData.voiceOutputSelector == nullptr) != (renderData.globalOutputSelector == nullptr));
+
+  auto graphData = renderData.graphData;
+  auto renderState = graphData->renderState;
+  auto moduleProcState = renderState->ModuleProcState();
+  auto scalarState = renderState->ScalarContainer()->Raw();
+  auto exchangeState = renderState->ExchangeContainer()->Raw();
+  auto const& moduleExchange = moduleExchangeSelector(exchangeState);
+
+  // TODO per voice
+  graphData->text = "OFF";
+  if (!moduleExchange.active)
+    return;
+
+  float guiSampleCount = (float)graphData->pixelWidth;
+  float dspSampleCount = (float)moduleExchange.lengthSamples;
+  float dspSampleRate = renderState->ExchangeContainer()->SampleRate();
+  moduleProcState->sampleRate = dspSampleRate / (dspSampleCount / guiSampleCount);  
+
+  renderState->PrepareForRenderPrimary();
+  FBRenderModuleGraphSeries(renderData, graphData->primarySeries);
+  float durationSeconds = renderData.graphData->primarySeries.size() / moduleProcState->sampleRate;
+  renderData.graphData->text = FBFormatFloat(durationSeconds, FBDefaultDisplayPrecision) + " Sec";
+  
+  renderState->PrepareForRenderExchange();  
+  assert(moduleExchange.positionSamples < moduleExchange.lengthSamples);
+  float positionNormalized = moduleExchange.positionSamples / (float)moduleExchange.lengthSamples;
+
+  // TODO voice
+  if (renderState->GlobalModuleExchangeStateEqualsPrimary((int)FFModuleType::GLFO, moduleProcState->moduleSlot))
+  {
+    graphData->primaryMarkers.push_back((int)(positionNormalized * graphData->primarySeries.size()));
+    assert(graphData->primaryMarkers[graphData->primaryMarkers.size() - 1] < graphData->primarySeries.size());
+    return;
+  }
+
+  auto& secondary = graphData->secondarySeries.emplace_back();
+  FBRenderModuleGraphSeries(renderData, secondary.points);
+  secondary.marker = (int)(positionNormalized * secondary.points.size());
+  assert(secondary.marker < secondary.points.size());
 }
