a67d03a5c4207a30b0a8025928b3c6700222b4f8
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-02-17 18:43:54 +0100

    1480

--- src/playground_base/playground_base/gui/controls/FBGUIParamToggleButton.cpp
+++ src/playground_base/playground_base/gui/controls/FBGUIParamToggleButton.cpp
@@ -12,7 +12,7 @@ FBGUIParamControl(plugGUI, param)
 {
   auto* context = plugGUI->HostContext();
   float normalized = context->GetParamNormalized(param->runtimeParamIndex);
-  SetValueNormalizedFromHost(normalized);
+  // TODO SetValueNormalizedFromHost(normalized);
   _isOn = getToggleState();
 }
 
@@ -25,27 +25,30 @@ FBGUIParamToggleButton::parentHierarchyChanged()
 String
 FBGUIParamToggleButton::getTooltip()
 {
+  // TODO 
   return _plugGUI->GetTooltipForParam(_param->runtimeParamIndex);
 }
 
+/* TODO
 void
 FBGUIParamToggleButton::SetValueNormalizedFromHost(float normalized)
 {
-  int plain = _param->static_.Boolean().NormalizedToPlain(normalized);
-  setToggleState(plain != 0, dontSendNotification);
-  _isOn = getToggleState();
+  // TODO int plain = _param->static_.Boolean().NormalizedToPlain(normalized);
+  // TODO setToggleState(plain != 0, dontSendNotification);
+  // TODO _isOn = getToggleState();
 }
+*/
 
 void
 FBGUIParamToggleButton::buttonStateChanged()
 {
-  float normalized = _param->static_.Boolean().PlainToNormalized(getToggleState());
-  int plain = _param->static_.Boolean().NormalizedToPlain(normalized);
-  if (_isOn != (plain != 0))
-  {
-    _plugGUI->HostContext()->PerformImmediateParamEdit(_param->runtimeParamIndex, normalized);
-    _plugGUI->SetParamNormalizedFromUI(_param->runtimeParamIndex, normalized);
-    _plugGUI->SteppedParamNormalizedChanged(_param->runtimeParamIndex);
-  }
-  _isOn = getToggleState();
+  // TODO float normalized = _param->static_.Boolean().PlainToNormalized(getToggleState());
+  // TODO int plain = _param->static_.Boolean().NormalizedToPlain(normalized);
+  // TODO if (_isOn != (plain != 0))
+// TODO   {
+// TODO     _plugGUI->HostContext()->PerformImmediateParamEdit(_param->runtimeParamIndex, normalized);
+// TODO     _plugGUI->SetParamNormalizedFromUI(_param->runtimeParamIndex, normalized);
+// TODO     _plugGUI->SteppedParamNormalizedChanged(_param->runtimeParamIndex);
+  // TODO }
+  // TODO _isOn = getToggleState();
 }
