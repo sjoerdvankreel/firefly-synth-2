3c0d6bdefd49730baf2713476ef7e42fe1de9848
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-02-17 21:45:55 +0100

    1490

---
+++ src/playground_base/playground_base/gui/controls/FBGUIParamComboBox.cpp
@@ -0,0 +1,34 @@
+#include <playground_base/gui/shared/FBPlugGUI.hpp>
+#include <playground_base/gui/glue/FBHostGUIContext.hpp>
+#include <playground_base/gui/controls/FBGUIParamComboBox.hpp>
+#include <playground_base/base/topo/runtime/FBRuntimeGUIParam.hpp>
+
+using namespace juce;
+
+FBGUIParamComboBox::
+FBGUIParamComboBox(FBPlugGUI* plugGUI, FBRuntimeGUIParam const* param):
+FBAutoSizeComboBox(param->static_.MakePopupMenu()),
+FBGUIParamControl(plugGUI, param)
+{
+  SetValueNormalizedFromPlug(plugGUI->HostContext()->GetGUIParamNormalized(param->runtimeParamIndex));
+}
+
+String
+FBGUIParamComboBox::getTooltip()
+{
+  return _plugGUI->GetTooltipForGUIParam(_param->runtimeParamIndex);
+}
+
+void
+FBGUIParamComboBox::SetValueNormalizedFromPlug(float normalized)
+{
+  int plain = _param->static_.NormalizedToAnyDiscreteSlow(normalized);
+  setSelectedId(plain + 1, dontSendNotification);
+}
+
+void
+FBGUIParamComboBox::valueChanged(Value& value)
+{
+  float normalized = _param->static_.AnyDiscreteToNormalizedSlow(getSelectedId() - 1);
+  _plugGUI->HostContext()->SetGUIParamNormalized(_param->runtimeParamIndex, normalized);
+}
---
+++ src/playground_base/playground_base/gui/controls/FBGUIParamComboBox.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <playground_base/gui/shared/FBGUIParamControl.hpp>
+#include <playground_base/gui/controls/FBAutoSizeComboBox.hpp>
+#include <juce_gui_basics/juce_gui_basics.h>
+
+class FBPlugGUI;
+struct FBRuntimeGUIParam;
+
+class FBGUIParamComboBox final:
+public FBAutoSizeComboBox,
+public FBGUIParamControl
+{
+  int _maxTextWidth = 0;
+
+public:
+  juce::String getTooltip() override;
+  void valueChanged(juce::Value& value) override;
+  void SetValueNormalizedFromPlug(float normalized) override;
+  FBGUIParamComboBox(FBPlugGUI* plugGUI, FBRuntimeGUIParam const* param);
+};
