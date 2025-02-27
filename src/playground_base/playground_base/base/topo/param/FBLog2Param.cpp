fc6dd3c55ce4a1bb820b1253023d672501da2ac3
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-02-27 21:53:06 +0100

    1545

--- src/playground_base/playground_base/base/topo/param/FBLog2Param.cpp
+++ src/playground_base/playground_base/base/topo/param/FBLog2Param.cpp
@@ -1,32 +1,44 @@
 #include <playground_base/base/shared/FBFormat.hpp>
 #include <playground_base/base/topo/param/FBLog2Param.hpp>
 
-float
-FBLog2Param::PlainToNormalized(float plain) const
-{
-  float result = std::log2((plain - _offset) / _curveStart) / _expo;
-  assert(0.0f <= result && result <= 1.0f);
-  return result;
-}
-
-void 
-FBLog2Param::Init(float offset, float curveStart, float curveEnd)
+void
+FBLog2ParamRealTime::Init(float offset, float curveStart, float curveEnd)
 {
   _offset = offset;
   _curveStart = curveStart;
   _expo = std::log(curveEnd / curveStart) / std::log(2.0f);
 }
 
-std::string
-FBLog2Param::PlainToText(FBValueTextDisplay display, float plain) const
+int 
+FBLog2ParamNonRealTime::ValueCount() const
 {
-  if(display == FBValueTextDisplay::IO)
+  return 0;
+}
+
+float 
+FBLog2ParamNonRealTime::PlainToNormalized(int plain) const
+{
+  float result = std::log2((plain - _offset) / _curveStart) / _expo;
+  assert(0.0f <= result && result <= 1.0f);
+  return result;
+}
+
+int
+FBLog2ParamNonRealTime::NormalizedToPlain(float normalized) const
+{
+  return FBLog2ParamRealTime::NormalizedToPlain(normalized);
+}
+
+std::string
+FBLog2ParamNonRealTime::PlainToText(FBValueTextDisplay display, int plain) const
+{
+  if (display == FBValueTextDisplay::IO)
     return std::to_string(plain);
   return FBFormatFloat(plain, FBDefaultDisplayPrecision);
-} 
+}
 
-std::optional<float>
-FBLog2Param::TextToPlain(std::string const& text) const
+std::optional<int>
+FBLog2ParamNonRealTime::TextToPlain(FBValueTextDisplay display, std::string const& text) const
 {
   char* end;
   float result = std::strtof(text.c_str(), &end);
--- src/playground_base/playground_base/base/topo/param/FBLog2Param.hpp
+++ src/playground_base/playground_base/base/topo/param/FBLog2Param.hpp
@@ -2,38 +2,46 @@
 
 #include <playground_base/dsp/shared/FBDSPUtility.hpp>
 #include <playground_base/base/shared/FBVector.hpp>
-#include <playground_base/base/topo/static/FBTextDisplay.hpp>
+#include <playground_base/base/shared/FBLifetime.hpp>
+#include <playground_base/base/topo/static/FBParamNonRealTime.hpp>
 
 #include <cmath>
 #include <string>
 #include <optional>
 #include <algorithm>
 
-struct FBLog2Param
+struct FBLog2ParamRealTime
 {
-private:
+protected:
   float _expo = {};
   float _offset = {};
   float _curveStart = {};
 
 public:
+  FB_NOCOPY_NOMOVE_DEFCTOR(FBLog2ParamRealTime);
   void Init(float offset, float curveStart, float curveEnd);
 
-  float PlainToNormalized(float plain) const;
   float NormalizedToPlain(float normalized) const;
   FBFloatVector NormalizedToPlain(FBFloatVector normalized) const;
   FBDoubleVector NormalizedToPlain(FBDoubleVector normalized) const;
-
   int NormalizedTimeToSamples(float normalized, float sampleRate) const;
   int NormalizedFreqToSamples(float normalized, float sampleRate) const;
+};
 
-  int ValueCount() const { return 0; }
-  std::optional<float> TextToPlain(std::string const& text) const;
-  std::string PlainToText(FBValueTextDisplay display, float plain) const;
+struct FBLog2ParamNonRealTime final:
+public FBLog2ParamRealTime,
+public IFBParamNonRealTime
+{
+  FB_NOCOPY_NOMOVE_DEFCTOR(FBLog2ParamNonRealTime);
+  int ValueCount() const override;
+  float PlainToNormalized(int plain) const override;
+  int NormalizedToPlain(float normalized) const override;
+  std::string PlainToText(FBValueTextDisplay display, int plain) const override;
+  std::optional<int> TextToPlain(FBValueTextDisplay display, std::string const& text) const override;
 };
 
 inline float
-FBLog2Param::NormalizedToPlain(float normalized) const
+FBLog2ParamRealTime::NormalizedToPlain(float normalized) const
 {
   float result = _offset + _curveStart * std::pow(2.0f, _expo * normalized);
   assert(result >= _curveStart + _offset);
@@ -41,25 +49,25 @@ FBLog2Param::NormalizedToPlain(float normalized) const
 }
 
 inline FBFloatVector 
-FBLog2Param::NormalizedToPlain(FBFloatVector normalized) const
+FBLog2ParamRealTime::NormalizedToPlain(FBFloatVector normalized) const
 {
   return _offset + _curveStart * xsimd::pow(FBFloatVector(2.0f), _expo * normalized);
 }
 
 inline FBDoubleVector 
-FBLog2Param::NormalizedToPlain(FBDoubleVector normalized) const
+FBLog2ParamRealTime::NormalizedToPlain(FBDoubleVector normalized) const
 {
   return _offset + _curveStart * xsimd::pow(FBDoubleVector(2.0), _expo * normalized);
 }
 
 inline int
-FBLog2Param::NormalizedTimeToSamples(float normalized, float sampleRate) const
+FBLog2ParamRealTime::NormalizedTimeToSamples(float normalized, float sampleRate) const
 {
   return FBTimeToSamples(NormalizedToPlain(normalized), sampleRate);
 }
 
 inline int
-FBLog2Param::NormalizedFreqToSamples(float normalized, float sampleRate) const
+FBLog2ParamRealTime::NormalizedFreqToSamples(float normalized, float sampleRate) const
 {
   return FBFreqToSamples(NormalizedToPlain(normalized), sampleRate);
 }
