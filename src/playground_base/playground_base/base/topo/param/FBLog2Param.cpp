bfd7c775f9b55d5d6e732114ad83e1c3040c4b28
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-03-03 22:31:18 +0100

    Revert "1545"
    This reverts commit fc6dd3c55ce4a1bb820b1253023d672501da2ac3.

--- src/playground_base/playground_base/base/topo/param/FBLog2Param.cpp
+++ src/playground_base/playground_base/base/topo/param/FBLog2Param.cpp
@@ -1,44 +1,32 @@
 #include <playground_base/base/shared/FBFormat.hpp>
 #include <playground_base/base/topo/param/FBLog2Param.hpp>
 
-void
-FBLog2ParamRealTime::Init(float offset, float curveStart, float curveEnd)
-{
-  _offset = offset;
-  _curveStart = curveStart;
-  _expo = std::log(curveEnd / curveStart) / std::log(2.0f);
-}
-
-int 
-FBLog2ParamNonRealTime::ValueCount() const
-{
-  return 0;
-}
-
-float 
-FBLog2ParamNonRealTime::PlainToNormalized(int plain) const
+float
+FBLog2Param::PlainToNormalized(float plain) const
 {
   float result = std::log2((plain - _offset) / _curveStart) / _expo;
   assert(0.0f <= result && result <= 1.0f);
   return result;
 }
 
-int
-FBLog2ParamNonRealTime::NormalizedToPlain(float normalized) const
+void 
+FBLog2Param::Init(float offset, float curveStart, float curveEnd)
 {
-  return FBLog2ParamRealTime::NormalizedToPlain(normalized);
+  _offset = offset;
+  _curveStart = curveStart;
+  _expo = std::log(curveEnd / curveStart) / std::log(2.0f);
 }
 
 std::string
-FBLog2ParamNonRealTime::PlainToText(FBValueTextDisplay display, int plain) const
+FBLog2Param::PlainToText(FBValueTextDisplay display, float plain) const
 {
-  if (display == FBValueTextDisplay::IO)
+  if(display == FBValueTextDisplay::IO)
     return std::to_string(plain);
   return FBFormatFloat(plain, FBDefaultDisplayPrecision);
-}
+} 
 
-std::optional<int>
-FBLog2ParamNonRealTime::TextToPlain(FBValueTextDisplay display, std::string const& text) const
+std::optional<float>
+FBLog2Param::TextToPlain(std::string const& text) const
 {
   char* end;
   float result = std::strtof(text.c_str(), &end);
