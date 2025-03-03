86d39037b6affed69e3309843d3f66b8ab100eda
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-03-03 22:31:18 +0100

    Revert "1543"
    This reverts commit 44696a01375f3beed2474509582525fd7eab225a.

--- src/playground_base/playground_base/base/topo/param/FBDiscreteLog2Param.cpp
+++ src/playground_base/playground_base/base/topo/param/FBDiscreteLog2Param.cpp
@@ -1,34 +1,21 @@
 #include <playground_base/base/topo/param/FBDiscreteLog2Param.hpp>
-
 #include <bit>
 
-int 
-FBDiscreteLog2ParamNonRealTime::ValueCount() const
+std::string
+FBDiscreteLog2Param::PlainToText(int plain) const
 {
-  return valueCount;
+  return std::to_string(plain);
 }
 
-float 
-FBDiscreteLog2ParamNonRealTime::PlainToNormalized(int plain) const
+float
+FBDiscreteLog2Param::PlainToNormalized(int plain) const
 {
   plain = std::bit_width((unsigned int)plain) - 1;
   return std::clamp(plain / (valueCount - 1.0f), 0.0f, 1.0f);
 }
 
-int
-FBDiscreteLog2ParamNonRealTime::NormalizedToPlain(float normalized) const
-{
-  return FBDiscreteLog2ParamRealTime::NormalizedToPlain(normalized);
-}
-
-std::string
-FBDiscreteLog2ParamNonRealTime::PlainToText(FBValueTextDisplay display, int plain) const
-{
-  return std::to_string(plain);
-}
-
 std::optional<int>
-FBDiscreteLog2ParamNonRealTime::TextToPlain(FBValueTextDisplay display, std::string const& text) const
+FBDiscreteLog2Param::TextToPlain(std::string const& text) const
 {
   char* end;
   unsigned long plain = std::strtoul(text.c_str(), &end, 10);
--- src/playground_base/playground_base/base/topo/param/FBDiscreteLog2Param.hpp
+++ src/playground_base/playground_base/base/topo/param/FBDiscreteLog2Param.hpp
@@ -1,33 +1,22 @@
 #pragma once
 
-#include <playground_base/base/shared/FBLifetime.hpp>
-#include <playground_base/base/topo/static/FBParamNonRealTime.hpp>
-
 #include <string>
 #include <optional>
 #include <algorithm>
 
-struct FBDiscreteLog2ParamRealTime
+struct FBDiscreteLog2Param
 {
   int valueCount = {};
-  int NormalizedToPlain(float normalized) const;
-  FB_NOCOPY_NOMOVE_DEFCTOR(FBDiscreteLog2ParamRealTime);
-};
+  int ValueCount() const { return valueCount; }
 
-struct FBDiscreteLog2ParamNonRealTime:
-public FBDiscreteLog2ParamRealTime,
-public IFBParamNonRealTime
-{
-  FB_NOCOPY_NOMOVE_DEFCTOR(FBDiscreteLog2ParamNonRealTime);
-  int ValueCount() const override;
-  float PlainToNormalized(int plain) const override;
-  int NormalizedToPlain(float normalized) const override;
-  std::string PlainToText(FBValueTextDisplay display, int plain) const override;
-  std::optional<int> TextToPlain(FBValueTextDisplay display, std::string const& text) const override;
+  float PlainToNormalized(int plain) const;
+  int NormalizedToPlain(float normalized) const;
+  std::string PlainToText(int plain) const;
+  std::optional<int> TextToPlain(std::string const& text) const;
 };
 
 inline int
-FBDiscreteLog2ParamRealTime::NormalizedToPlain(float normalized) const
+FBDiscreteLog2Param::NormalizedToPlain(float normalized) const
 {
   return 1 << std::clamp((int)(normalized * valueCount), 0, valueCount - 1);
 }
