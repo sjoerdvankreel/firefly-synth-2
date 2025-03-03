745e1bfba23e9b6ce9a2c8cad86a26a3c63e7e95
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-03-03 22:31:18 +0100

    Revert "1555"
    This reverts commit 82a892d53fab3ba4d87254f2e0bc2b1aa899619e.

--- src/playground_base/playground_base/base/topo/param/FBNoteParam.hpp
+++ src/playground_base/playground_base/base/topo/param/FBNoteParam.hpp
@@ -2,7 +2,7 @@
 
 #include <playground_base/base/shared/FBLifetime.hpp>
 #include <playground_base/base/topo/param/FBListItem.hpp>
-#include <playground_base/base/topo/param/FBItemsParamNonRealTime.hpp>
+#include <playground_base/base/topo/static/FBListParamNonRealTime.hpp>
 
 #include <vector>
 #include <string>
@@ -18,11 +18,13 @@ struct FBNoteParamRealTime
   FB_NOCOPY_NOMOVE_DEFCTOR(FBNoteParamRealTime);
 };
 
-struct FBNoteParamNonRealTime final:
+struct FBNoteParamNonRealTime:
 public FBNoteParamRealTime,
-public FBItemsParamNonRealTime
+public IFBListParamNonRealTime
 {
   FB_NOCOPY_NOMOVE_DEFCTOR(FBNoteParamNonRealTime);
+  bool IsList() const override;
+  bool IsStepped() const override;
   int ValueCount() const override;
   juce::PopupMenu MakePopupMenu() const override;
   float PlainToNormalized(int plain) const override;
