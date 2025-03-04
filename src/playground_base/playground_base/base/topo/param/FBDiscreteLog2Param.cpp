d708eb10ba6e138eff39c64e52d1c635bf61243c
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-03-04 22:11:04 +0100

    Revert "1573 TEMP GIVE UP IM TOO STUPID FOR MATH"
    This reverts commit 98a77853f7be204eaa937d02d7f1d6626b003ad8.

--- src/playground_base/playground_base/base/topo/param/FBDiscreteLog2Param.cpp
+++ src/playground_base/playground_base/base/topo/param/FBDiscreteLog2Param.cpp
@@ -24,10 +24,7 @@ FBDiscreteLog2ParamNonRealTime::ValueCount() const
 double
 FBDiscreteLog2ParamNonRealTime::NormalizedToPlain(double normalized) const 
 {
-  auto plain = static_cast<double>(1 << std::clamp((int)std::round(normalized * valueCount), 0, valueCount - 1));
-  auto norm = PlainToNormalized(plain);
-  //auto plain2 = NormalizedToPlain(norm);
-  return plain;
+  return static_cast<double>(1 << std::clamp((int)std::round(normalized * valueCount), 0, valueCount - 1));
 }
 
 std::string
@@ -48,9 +45,7 @@ FBDiscreteLog2ParamNonRealTime::PlainToNormalized(double plain) const
     linear++;
     discrete /= 2;
   }
-  auto norm = std::clamp(linear / (valueCount - 1.0), 0.0, 1.0);
-  //auto pln = NormalizedToPlain(norm);
-  return norm;
+  return std::clamp(linear / (valueCount - 1.0), 0.0, 1.0);
 }
 
 std::optional<double>
