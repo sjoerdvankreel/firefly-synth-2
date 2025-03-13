7ced7237156549e6005cdac66810f9095f785b53
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-03-13 22:21:45 +0100

    1696

--- src/playground_plug/playground_plug/modules/osci_am/FFOsciAMGUI.cpp
+++ src/playground_plug/playground_plug/modules/osci_am/FFOsciAMGUI.cpp
@@ -16,16 +16,16 @@ static Component*
 MakeSectionAll(FBPlugGUI* plugGUI, int moduleSlot)
 {
   auto topo = plugGUI->HostContext()->Topo();
-  auto grid = plugGUI->StoreComponent<FBGridComponent>(FBGridType::Module, 2, FFOsciAMSlotCount * 2);
+  auto grid = plugGUI->StoreComponent<FBGridComponent>(FBGridType::Module, 3, FFOsciAMSlotCount * 2);
   for (int i = 0; i < FFOsciAMSlotCount; i++)
   {
     auto on = topo->audio.ParamAtTopo({ (int)FFModuleType::OsciAM, 0, (int)FFOsciAMParam::On, i });
     grid->Add(0, i * 2, plugGUI->StoreComponent<FBParamLabel>(plugGUI, on));
     grid->Add(0, i * 2 + 1, plugGUI->StoreComponent<FBParamToggleButton>(plugGUI, on));
     auto mix = topo->audio.ParamAtTopo({ (int)FFModuleType::OsciAM, 0, (int)FFOsciAMParam::Mix, i });
-    grid->Add(1, i * 2, plugGUI->StoreComponent<FBParamSlider>(plugGUI, mix, Slider::SliderStyle::RotaryVerticalDrag));
+    grid->Add(1, i * 2, 1, 2, plugGUI->StoreComponent<FBParamSlider>(plugGUI, mix, Slider::SliderStyle::LinearHorizontal));
     auto ring = topo->audio.ParamAtTopo({ (int)FFModuleType::OsciAM, 0, (int)FFOsciAMParam::Ring, i });
-    grid->Add(1, i * 2 + 1, plugGUI->StoreComponent<FBParamSlider>(plugGUI, ring, Slider::SliderStyle::RotaryVerticalDrag));
+    grid->Add(2, i * 2, 1, 2, plugGUI->StoreComponent<FBParamSlider>(plugGUI, ring, Slider::SliderStyle::LinearHorizontal));
   }
   return grid;
 }
