ffc29b9c8380062010d9ab62c47559d44578cfcd
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-03-15 16:09:16 +0100

    1707

--- src/playground_plug/playground_plug/modules/osci_am/FFOsciAMGUI.cpp
+++ src/playground_plug/playground_plug/modules/osci_am/FFOsciAMGUI.cpp
@@ -2,12 +2,13 @@
 #include <playground_plug/modules/osci_am/FFOsciAMTopo.hpp>
 
 #include <playground_base/base/topo/runtime/FBRuntimeTopo.hpp>
-#include <playground_base/gui/components/FBGridComponent.hpp>
 #include <playground_base/gui/shared/FBPlugGUI.hpp>
 #include <playground_base/gui/glue/FBHostGUIContext.hpp>
 #include <playground_base/gui/controls/FBParamLabel.hpp>
 #include <playground_base/gui/controls/FBParamSlider.hpp>
 #include <playground_base/gui/controls/FBParamToggleButton.hpp>
+#include <playground_base/gui/components/FBGridComponent.hpp>
+#include <playground_base/gui/components/FBSectionComponent.hpp>
 #include <playground_base/gui/components/FBModuleTabComponent.hpp>
 
 using namespace juce;
@@ -41,13 +42,15 @@ MakeSectionAll(FBPlugGUI* plugGUI, int moduleSlot)
     grid->Add(1, 1 + i * 2 + 1, plugGUI->StoreComponent<FBParamSlider>(plugGUI, ring, Slider::SliderStyle::RotaryVerticalDrag));
   }
   int x = 0;
+  int w = 2;
   grid->MarkSection({ 0, 0, 2, 1 });
   for (int i = 0; i < FFOsciCount; i++)
   {
-    grid->MarkSection({ 0, 1 + x, 2, 1 });
+    grid->MarkSection({ 0, 1 + x, 2, w });
     x += 2 * (i + 1);
+    w += 2;
   }
-  return grid;
+  return plugGUI->StoreComponent<FBSectionComponent>(grid);
 }
 
 Component*
