7d0556c39d34148b9b7361da03033bf810125dfd
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-04-06 19:54:17 +0200

    1915

--- src/playground_plug/playground_plug/shared/FFPlugTopo.hpp
+++ src/playground_plug/playground_plug/shared/FFPlugTopo.hpp
@@ -7,7 +7,7 @@ std::unique_ptr<FBStaticTopo> FFMakeTopo();
 
 enum class FFModuleType { GLFO, GFilter, Osci, OsciMod, Env, Master, GUISettings, Count }; 
 
-inline int constexpr FFEnvCount = 10;
+inline int constexpr FFEnvCount = 8;
 inline int constexpr FFOsciCount = 4;
 inline int constexpr FFGLFOCount = 1;
 inline int constexpr FFGFilterCount = 2;
