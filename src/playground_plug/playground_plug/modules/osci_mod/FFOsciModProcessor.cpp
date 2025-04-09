eaa843572d534a937121834110fe9a13c7b4487f
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-04-09 19:07:56 +0200

    1949 temp?

--- src/playground_plug/playground_plug/modules/osci_mod/FFOsciModProcessor.cpp
+++ src/playground_plug/playground_plug/modules/osci_mod/FFOsciModProcessor.cpp
@@ -41,7 +41,7 @@ FFOsciModProcessor::Process(FBModuleProcState& state)
     if (_voiceState.fmMode[i] != FFOsciModFMMode::Off)
     {
       auto const& fmIndexNorm = procParams.acc.fmIndex[i].Voice()[voice];
-      topo.NormalizedToLog2Fast(FFOsciModParam::FMIndex, fmIndexNorm, outputFMIndex[i]);
+      topo.NormalizedToLinearFast(FFOsciModParam::FMIndex, fmIndexNorm, outputFMIndex[i]);
     }
   }
 
