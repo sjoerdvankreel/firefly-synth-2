16ef19cdb3699e30fd8c2d660134dc301ec94cbf
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-05-06 23:07:47 +0200

    2199 temp

--- src/playground_base/playground_base/gui/shared/FBGUIConfig.hpp
+++ src/playground_base/playground_base/gui/shared/FBGUIConfig.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 inline int constexpr FBGUIFPS = 30;
+inline int constexpr FBGUIFontSize = 15;
--- src/playground_plug/playground_plug/modules/osci_mod/FFOsciModTopo.cpp
+++ src/playground_plug/playground_plug/modules/osci_mod/FFOsciModTopo.cpp
@@ -18,6 +18,21 @@ FFOsciModFormatSlot(int slot)
   }
 }
 
+std::string
+FFOsciModFormatSlotVertical(int slot)
+{
+  switch (slot)
+  {
+  case 0: return "2\r\n^\r\n1";
+  case 1: return "3\r\n^\r\n1";
+  case 2: return "3\r\n^\r\n2";
+  case 3: return "4\r\n^\r\n1";
+  case 4: return "4\r\n^\r\n2";
+  case 5: return "4\r\n^\r\n3";
+  default: assert(false); return "";
+  }
+}
+
 std::unique_ptr<FBStaticModule>
 FFMakeOsciModTopo()
 {
