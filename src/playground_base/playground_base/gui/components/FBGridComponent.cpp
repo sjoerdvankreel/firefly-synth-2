2dae3cfffbfd83b0cc30cd22fef40dccf0364ac6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-05-10 16:37:42 +0200

    2271

--- src/playground_base/playground_base/gui/components/FBGridComponent.cpp
+++ src/playground_base/playground_base/gui/components/FBGridComponent.cpp
@@ -220,6 +220,6 @@ FBGridComponent::paint(Graphics& g)
       if (_grid.items[j].column.end.getNumber() == _sections[i].pos.col + _sections[i].span.col + 1)
         x1 = static_cast<float>(_grid.items[j].associatedComponent->getRight());
     }
-    g.fillRoundedRectangle(x0, y0, x1 - x0, y1 - y0, 2.0f); // TODO 2
+    g.fillRoundedRectangle(x0, y0, x1 - x0, y1 - y0, 2.0f);
   }
 }
