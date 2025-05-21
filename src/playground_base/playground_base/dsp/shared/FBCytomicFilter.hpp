51c46114cbe39e22b432699ca159ab2f0730268c
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-05-21 18:53:04 +0200

    2438

--- src/playground_base/playground_base/dsp/shared/FBCytomicFilter.hpp
+++ src/playground_base/playground_base/dsp/shared/FBCytomicFilter.hpp
@@ -4,6 +4,7 @@
 
 #include <array>
 #include <cmath>
+#include <numbers>
 #include <cassert>
 
 enum class FBCytomicFilterMode
@@ -36,12 +37,12 @@ FBCytomicFilter<Channels>::Next(
   int channel, double in)
 {
   double v0 = in;
-  double v3 = v0 - _ic2eq[channel];
-  double v1 = _a1 * _ic1eq[chhannel] + _a2 * v3;
-  double v2 = _ic2eq[channel] + _a2 * _ic1eq[channel] + _a3 * v3;
-  _ic1eq[channel] = 2 * v1 - _ic1eq[channel];
-  _ic2eq[channel] = 2 * v2 - _ic2eq[channel];
-  return _m0 * v0 + _m1 * v1 + _m2 * v2;
+  double v3 = v0 - this->_ic2eq[channel];
+  double v1 = this->_a1 * this->_ic1eq[channel] + this->_a2 * v3;
+  double v2 = this->_ic2eq[channel] + this->_a2 * this->_ic1eq[channel] + this->_a3 * v3;
+  this->_ic1eq[channel] = 2 * v1 - this->_ic1eq[channel];
+  this->_ic2eq[channel] = 2 * v2 - this->_ic2eq[channel];
+  return this->_m0 * v0 + this->_m1 * v1 + this->_m2 * v2;
 }
 
 template <int Channels>
@@ -50,12 +51,13 @@ FBCytomicFilter<Channels>::Set(
   FBCytomicFilterMode mode, double sampleRate, 
   double freqHz, double resNorm, double gainDb)
 {
+  double a;
   double k = 2.0 - 2.0 * resNorm;
-  double g = std::tan(std::numbers::pi * freq / sampleRate);
+  double g = std::tan(std::numbers::pi * freqHz / sampleRate);
 
   if (mode >= FBCytomicFilterMode::BLL)
   {
-    double a = std::pow(10.0, gainDb / 40.0);
+    a = std::pow(10.0, gainDb / 40.0);
     switch (mode)
     {
     case FBCytomicFilterMode::BLL: k = k / a; break;
@@ -65,24 +67,24 @@ FBCytomicFilter<Channels>::Set(
     }
   }
 
-  _a1 = 1.0 / (1.0 + g * (g + k));
-  _a2 = g * _a1;
-  _a3 = g * _a2;
+  this->_a1 = 1.0 / (1.0 + g * (g + k));
+  this->_a2 = g * this->_a1;
+  this->_a3 = g * this->_a2;
 
-  _m0 = 0.0f;
-  _m1 = 0.0f;
-  _m2 = 0.0f;
+  this->_m0 = 0.0f;
+  this->_m1 = 0.0f;
+  this->_m2 = 0.0f;
   switch (mode)
   {
-  case FBCytomicFilterMode::LPF: m2_ = 1.0; break;
-  case FBCytomicFilterMode::BPF: m1_ = 1.0; break;
-  case FBCytomicFilterMode::HPF: m0_ = 1.0; m1_ = -k; m2_ = -1.0; break;
-  case FBCytomicFilterMode::BSF: m0_ = 1.0; m1_ = -k; break;
-  case FBCytomicFilterMode::PEQ: m0_ = 1.0; m1_ = -k; m2_ = -2.0; break;
-  case FBCytomicFilterMode::APF: m0_ = 1.0; m1_ = -2.0 * k; break;
-  case FBCytomicFilterMode::BLL: m0_ = 1.0; m1_ = k * (a * a - 1.0); break;
-  case FBCytomicFilterMode::LSH: m0_ = 1.0; m1_ = k * (a - 1.0); m2_ = a * a - 1.0; break;
-  case FBCytomicFilterMode::HSH: m0_ = a * a; m1_ = k * (1.0 - a * a); m2_ = 1.0 - a * a; break;
+  case FBCytomicFilterMode::LPF: this->_m2 = 1.0; break;
+  case FBCytomicFilterMode::BPF: this->_m1 = 1.0; break;
+  case FBCytomicFilterMode::HPF: this->_m0 = 1.0; this->_m1 = -k; this->_m2 = -1.0; break;
+  case FBCytomicFilterMode::BSF: this->_m0 = 1.0; this->_m1 = -k; break;
+  case FBCytomicFilterMode::PEQ: this->_m0 = 1.0; this->_m1 = -k; this->_m2 = -2.0; break;
+  case FBCytomicFilterMode::APF: this->_m0 = 1.0; this->_m1 = -2.0 * k; break;
+  case FBCytomicFilterMode::BLL: this->_m0 = 1.0; this->_m1 = k * (a * a - 1.0); break;
+  case FBCytomicFilterMode::LSH: this->_m0 = 1.0; this->_m1 = k * (a - 1.0); this->_m2 = a * a - 1.0; break;
+  case FBCytomicFilterMode::HSH: this->_m0 = a * a; this->_m1 = k * (1.0 - a * a); this->_m2 = 1.0 - a * a; break;
   default: assert(false);  break;
   }
 }
