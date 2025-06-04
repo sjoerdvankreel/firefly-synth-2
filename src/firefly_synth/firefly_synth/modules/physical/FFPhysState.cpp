42d85e98310d463d2a507f6cfa6c29712844ea39
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-06-04 14:40:32 +0200

    2621

--- src/firefly_synth/firefly_synth/modules/physical/FFPhysState.cpp
+++
@@ -1,9 +0,0 @@
-#include <firefly_synth/modules/physical/FFPhysState.hpp>
-#include <firefly_synth/modules/physical/FFPhysProcessor.hpp>
-
-FFPhysDSPState::
-~FFPhysDSPState() {}
-
-FFPhysDSPState::
-FFPhysDSPState() :
-processor(std::make_unique<FFPhysProcessor>()) {}
