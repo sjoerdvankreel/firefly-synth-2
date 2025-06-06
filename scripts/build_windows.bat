77fd4d1e34ee4d91a6c665d9138374d92be16ca8
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-06-06 21:20:23 +0200

    zip it (windows)

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -11,6 +11,13 @@ if %errorlevel% neq 0 exit /b !errorlevel!
 msbuild /property:Configuration="%1" firefly_synth_2.sln
 if %errorlevel% neq 0 exit /b !errorlevel!
 
+cd ..\..\dist\windows\"%1"
+7z a -tzip FireflySynth2-windows-vst3-"%1".zip FireflySynth2.vst3\*
+7z a -tzip FireflySynth2-windows-clap-"%1".zip FireflySynth2.clap\*
+if %errorlevel% neq 0 exit /b !errorlevel!
+
+cd ..\..\..\..\scripts
+
 exit /b 0
 :usage
 echo "Usage: build_windows Debug|RelWithDebInfo|Release"
