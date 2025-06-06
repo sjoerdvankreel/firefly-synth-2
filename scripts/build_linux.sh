eee7d585bb300e8f848b884e0a26028162ee8478
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-06-06 21:33:10 +0200

    zip it (linux)

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" == "" ]; then
-  echo "Usage: build_linux Debug|Release"
+  echo "Usage: build_linux Debug|RelWithDebInfo|Release"
   exit 1
 fi
 
@@ -11,3 +11,8 @@ mkdir -p "build/linux/$1"
 cd "build/linux/$1"
 cmake -DCMAKE_BUILD_TYPE="$1" ../../..
 make
+
+cd ../../../dist/linux/"$1"
+zip -r FireflySynth2-linux-vst3-"$1".zip FireflySynth2.vst3
+zip -r FireflySynth2-linux-clap-"$1".zip FireflySynth2.clap
+cd ../../../scripts
