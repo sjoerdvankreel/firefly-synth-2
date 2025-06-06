8eb4083119c0db95605bc336c56adedfceec8757
sjoerdvankreel@gmail.com <sjoerdvankreel@gmail.com>
2025-06-06 22:36:34 +0200

    zip it (mac)

--- scripts/build_mac.sh
+++ scripts/build_mac.sh
@@ -12,7 +12,7 @@ cd "build/mac/$1"
 cmake -DCMAKE_BUILD_TYPE="$1" ../../..
 make
 
-cd ../../../dist/mac/"$1"
+cd ../../../dist/Darwin/"$1"
 zip -r FireflySynth2-mac-vst3-"$1".zip FireflySynth2.vst3
 zip -r FireflySynth2-mac-clap-"$1".zip FireflySynth2.clap
 cd ../../../scripts
