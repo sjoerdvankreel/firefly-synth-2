a583b755950940c9e68e06e753dc0c8dfd0958f6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-06-09 20:13:21 +0200

    Revert "black magic ! this gets me trace for the clap too"
    This reverts commit 17e754d188775d9ce5dc6b1c0ab491fad14203b1.

--- src/firefly_base/firefly_base/base/shared/FBLogging.cpp
+++ src/firefly_base/firefly_base/base/shared/FBLogging.cpp
@@ -40,8 +40,7 @@ FBLogTerminate()
 void
 FBLogInit(FBStaticTopoMeta const& meta)
 {
-  auto uuid = juce::Uuid().toString().toStdString();
-  auto path = FBGetUserPluginDataFolder(meta) / (uuid + ".log");
+  auto path = FBGetUserPluginDataFolder(meta) / "lastrun.log";
   auto file = File(String(path.string()));
   _logger = std::make_unique<FileLogger>(file, meta.NameVersionAndFormat(), 0);
   FB_LOG_INFO("Initialized log.");
