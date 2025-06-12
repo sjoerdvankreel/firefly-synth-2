9ac235b5aa668ebbab578f1c641c3aa3e885668b
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-06-12 21:04:31 +0200

    pretty

--- src/firefly_synth/firefly_synth/shared/FFPlugGUI.cpp
+++ src/firefly_synth/firefly_synth/shared/FFPlugGUI.cpp
@@ -110,6 +110,7 @@ FFPlugGUI::SetupGUI()
   float availableHeight = static_cast<float>(totalHeight - vTabCount * tabHeight);
   std::vector<int> rowSizes = {};
   rowSizes.push_back(static_cast<int>(tabHeight + 1.0f / 9.0f * availableHeight));
+  rowSizes.push_back(static_cast<int>(tabHeight + 1.0f / 9.0f * availableHeight));
   rowSizes.push_back(static_cast<int>(tabHeight + 2.0f / 9.0f * availableHeight));
   rowSizes.push_back(static_cast<int>(tabHeight + 2.0f / 9.0f * availableHeight));
   rowSizes.push_back(static_cast<int>(tabHeight + 2.0f / 9.0f * availableHeight));
@@ -118,15 +119,15 @@ FFPlugGUI::SetupGUI()
 
   FB_LOG_INFO("Creating GUI components.");
   _graph = StoreComponent<FBModuleGraphComponent>(this, _graphRenderState.get());
-  _content = StoreComponent<FBGridComponent>(FBGridType::Generic, 0, -1, rowSizes, std::vector<int> { 0, 0, 0, 1 });
-  _content->Add(0, 0, 1, 1, FFMakeMasterGUI(this));
-  _content->Add(0, 1, 1, 1, FFMakeOutputGUI(this));
-  _content->Add(0, 2, 1, 1, FFMakeGUISettingsGUI(this));
-  _content->Add(0, 3, 1, 1, _graph);
-  _content->Add(1, 0, 1, 4, FFMakeOscisGUI(this));
-  _content->Add(2, 0, 1, 4, FFMakeOsciModGUI(this));
-  _content->Add(3, 0, 1, 4, FFMakeEnvGUI(this));
-  _content->Add(4, 0, 1, 4, FFMakeEffectGUI(this));
+  _content = StoreComponent<FBGridComponent>(FBGridType::Generic, 0, -1, rowSizes, std::vector<int> { 1, 1, 1 });
+  _content->Add(0, 0, 1, 3, _graph);
+  _content->Add(1, 0, 1, 1, FFMakeMasterGUI(this));
+  _content->Add(1, 1, 1, 1, FFMakeOutputGUI(this));
+  _content->Add(1, 2, 1, 1, FFMakeGUISettingsGUI(this));
+  _content->Add(2, 0, 1, 3, FFMakeOscisGUI(this));
+  _content->Add(3, 0, 1, 3, FFMakeOsciModGUI(this));
+  _content->Add(4, 0, 1, 3, FFMakeEnvGUI(this));
+  _content->Add(5, 0, 1, 3, FFMakeEffectGUI(this));
   FB_LOG_INFO("Created GUI components.");
 
   FB_LOG_INFO("Making GUI visible.");
