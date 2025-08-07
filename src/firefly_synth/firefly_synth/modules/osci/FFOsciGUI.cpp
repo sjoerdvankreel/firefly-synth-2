c4ac723943153319c5286191ca475b8b2e5409e6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-08-07 19:08:02 +0200

    gui stuff

--- src/firefly_synth/firefly_synth/modules/osci/FFOsciGUI.cpp
+++ src/firefly_synth/firefly_synth/modules/osci/FFOsciGUI.cpp
@@ -84,7 +84,7 @@ MakeOsciSectionWave(FBPlugGUI* plugGUI, int moduleSlot)
 {
   FB_LOG_ENTRY_EXIT();
   auto topo = plugGUI->HostContext()->Topo();
-  auto grid = plugGUI->StoreComponent<FBGridComponent>(true, std::vector<int> { 1, 1 }, std::vector<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1 });
+  auto grid = plugGUI->StoreComponent<FBGridComponent>(true, std::vector<int> { 1, 1 }, std::vector<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1 });
   for (int i = 0; i < FFOsciWaveBasicCount; i++)
   {
     auto mode = topo->audio.ParamAtTopo({ { (int)FFModuleType::Osci, moduleSlot }, { (int)FFOsciParam::WaveBasicMode, i } });
