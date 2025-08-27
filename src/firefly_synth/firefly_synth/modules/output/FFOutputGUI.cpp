54ec273e933fe3478d2ff9e27200922cebffc1ea
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-08-27 12:19:10 +0200

    still fast

--- src/firefly_synth/firefly_synth/modules/output/FFOutputGUI.cpp
+++ src/firefly_synth/firefly_synth/modules/output/FFOutputGUI.cpp
@@ -8,7 +8,7 @@
 #include <firefly_base/gui/controls/FBLabel.hpp>
 #include <firefly_base/gui/controls/FBSlider.hpp>
 #include <firefly_base/gui/controls/FBComboBox.hpp>
-#include <firefly_base/gui/controls/FBOutputMeter.hpp>
+#include <firefly_base/gui/controls/FBOutputLabel.hpp>
 #include <firefly_base/gui/components/FBTabComponent.hpp>
 #include <firefly_base/gui/components/FBGridComponent.hpp>
 #include <firefly_base/gui/components/FBSectionComponent.hpp>
@@ -21,16 +21,16 @@ FFMakeOutputGUI(FBPlugGUI* plugGUI)
 {
   FB_LOG_ENTRY_EXIT();
   auto topo = plugGUI->HostContext()->Topo();
-  auto grid = plugGUI->StoreComponent<FBGridComponent>(true, std::vector<int> { 1 }, std::vector<int> { 0, 1, 0, 1, 0, 1 } );
-  auto cpu = topo->audio.ParamAtTopo({ { (int)FFModuleType::Output, 0 }, { (int)FFOutputParam::Cpu, 0 } });
-  grid->Add(0, 0, plugGUI->StoreComponent<FBParamLabel>(plugGUI, cpu));
-  grid->Add(0, 1, plugGUI->StoreComponent<FBOutputParamMeter>(plugGUI, cpu));
-  auto voices = topo->audio.ParamAtTopo({ { (int)FFModuleType::Output, 0 }, { (int)FFOutputParam::Voices, 0 } });
-  grid->Add(0, 2, plugGUI->StoreComponent<FBParamLabel>(plugGUI, voices));
-  grid->Add(0, 3, plugGUI->StoreComponent<FBOutputParamMeter>(plugGUI, voices));
+  auto grid = plugGUI->StoreComponent<FBGridComponent>(true, std::vector<int> { 1 }, std::vector<int> { 0, 0, 0, 0, 0, 0 });
   auto gain = topo->audio.ParamAtTopo({ { (int)FFModuleType::Output, 0 }, { (int)FFOutputParam::Gain, 0 } });
-  grid->Add(0, 4, plugGUI->StoreComponent<FBParamLabel>(plugGUI, gain));
-  grid->Add(0, 5, plugGUI->StoreComponent<FBOutputParamMeter>(plugGUI, gain));
+  grid->Add(0, 0, plugGUI->StoreComponent<FBParamLabel>(plugGUI, gain));
+  grid->Add(0, 1, plugGUI->StoreComponent<FBOutputParamLabel>(plugGUI, gain, "0", std::to_string(FFOutputMaxGain)));
+  auto cpu = topo->audio.ParamAtTopo({ { (int)FFModuleType::Output, 0 }, { (int)FFOutputParam::Cpu, 0 } });
+  grid->Add(0, 2, plugGUI->StoreComponent<FBParamLabel>(plugGUI, cpu));
+  grid->Add(0, 3, plugGUI->StoreComponent<FBOutputParamLabel>(plugGUI, cpu, "0", std::to_string(FFOutputMaxCpu)));
+  auto voices = topo->audio.ParamAtTopo({ { (int)FFModuleType::Output, 0 }, { (int)FFOutputParam::Voices, 0 } });
+  grid->Add(0, 4, plugGUI->StoreComponent<FBParamLabel>(plugGUI, voices));
+  grid->Add(0, 5, plugGUI->StoreComponent<FBOutputParamLabel>(plugGUI, voices, "0", std::to_string(FBMaxVoices)));
   grid->MarkSection({ { 0, 0 }, { 1, 6 } });
   auto section = plugGUI->StoreComponent<FBSubSectionComponent>(grid);
   return plugGUI->StoreComponent<FBSectionComponent>(section);
--- src/firefly_synth/firefly_synth/modules/output/FFOutputTopo.hpp
+++ src/firefly_synth/firefly_synth/modules/output/FFOutputTopo.hpp
@@ -4,6 +4,7 @@
 
 inline int constexpr FFOutputMaxCpu = 999;
 inline int constexpr FFOutputMaxGain = 999;
+inline int constexpr FFOutputSamplingRate = 3;
 
 struct FBStaticModule;
 std::unique_ptr<FBStaticModule> FFMakeOutputTopo();
