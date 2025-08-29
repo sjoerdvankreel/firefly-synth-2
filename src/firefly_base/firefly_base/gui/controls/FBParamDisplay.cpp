26e06e5abbf8916678dda37657c212858a610b6e
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-08-29 15:35:50 +0200

    take 36

--- src/firefly_base/firefly_base/gui/controls/FBParamDisplay.cpp
+++ src/firefly_base/firefly_base/gui/controls/FBParamDisplay.cpp
@@ -4,7 +4,7 @@
 #include <firefly_base/gui/glue/FBHostGUIContext.hpp>
 #include <firefly_base/base/topo/runtime/FBRuntimeParam.hpp>
 
-static int const MeterBarCount = 20;
+static int const MeterBarCount = 16;
 
 using namespace juce;
 
