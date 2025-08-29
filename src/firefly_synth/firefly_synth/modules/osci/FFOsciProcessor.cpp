d744d82260769f2b3807d5b08786a76d985c90bb
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-08-29 22:52:06 +0200

    take 54

--- src/firefly_synth/firefly_synth/modules/osci/FFOsciProcessor.cpp
+++ src/firefly_synth/firefly_synth/modules/osci/FFOsciProcessor.cpp
@@ -103,6 +103,8 @@ FFOsciProcessor::BeginVoice(bool graph, FBModuleProcState& state)
   double keyRetunedRatio = MTS_RetuningAsRatio(procState->mtsClient, (char)noteEvent.key, (char)noteEvent.channel);
   double keyRetunedSemisByRatio = 12.0 / std::log(2.0) * std::log(keyRetunedRatio);
   double keyFreqRetunedSemisByRatio = FBPitchToFreq((float)(_key + keyRetunedSemisByRatio));
+  double keyRetunedPitch = _key + keyRetunedSemis;
+  (void)keyRetunedPitch;
   // keyFreqRetunedDirect == keyFreqRetunedSemisFreq == keyFreqRetunedSemisByRatio
 
   (void)keyRetunedRatio;
@@ -115,7 +117,9 @@ FFOsciProcessor::BeginVoice(bool graph, FBModuleProcState& state)
   double keyPlus1FreqRetunedSemisFreq = FBPitchToFreq((float)(_key + 1 + keyPlus1RetunedSemis));
   double keyPlus1RetunedRatio = MTS_RetuningAsRatio(procState->mtsClient, (char)noteEvent.key + 1, (char)noteEvent.channel);
   double keyPlus1RetunedSemisByRatio = 12.0 / std::log(2.0) * std::log(keyPlus1RetunedRatio);
-  double keyPlus1FreqRetunedSemisByRatio = FBPitchToFreq((float)(_key + 1 + keyRetunedSemisByRatio));
+  double keyPlus1FreqRetunedSemisByRatio = FBPitchToFreq((float)(_key + 1 + keyPlus1RetunedSemisByRatio));
+  double keyPlus1RetunedPitch = _key + 1 + keyPlus1RetunedSemis;
+  (void)keyPlus1RetunedPitch;
   // keyPlus1FreqRetunedDirect == keyPlus1FreqRetunedSemisFreq == keyPlus1FreqRetunedSemisByRatio
 
   (void)keyPlus1RetunedRatio;
