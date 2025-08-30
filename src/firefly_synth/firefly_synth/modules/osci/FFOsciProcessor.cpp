17dcce26e94cb68e69fb170f0399e7d2e383e179
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-08-30 20:03:49 +0200

    take 77

--- src/firefly_synth/firefly_synth/modules/osci/FFOsciProcessor.cpp
+++ src/firefly_synth/firefly_synth/modules/osci/FFOsciProcessor.cpp
@@ -149,8 +149,10 @@ FFOsciProcessor::Process(bool graph, FBModuleProcState& state)
   auto& voiceState = procState->dsp.voice[voice];
   auto& output = voiceState.osci[state.moduleSlot].output;
   auto& uniOutputOversampled = voiceState.osci[state.moduleSlot].uniOutputOversampled;
-  auto const& pitchBendSemis = procState->dsp.global.master.bendAmountInSemis;
-  auto pitchBendTarget = procState->dsp.global.master.bendTarget;
+
+  auto masterPitchBendTarget = procState->dsp.global.master.bendTarget;
+  auto const& masterPitchBendSemis = procState->dsp.global.master.bendAmountInSemis;
+  auto const& voicePitchOffsetSemis = procState->dsp.voice[voice].voiceModule.pitchOffsetInSemis;
 
   output.Fill(0.0f);
   uniOutputOversampled.Fill(0.0f);
@@ -194,13 +196,13 @@ FFOsciProcessor::Process(bool graph, FBModuleProcState& state)
   {
     auto coarse = topo.NormalizedToLinearFast(FFOsciParam::Coarse, coarseNorm, s);
     auto fine = topo.NormalizedToLinearFast(FFOsciParam::Fine, fineNormModulated.Load(s));
-    auto pitch = _key + coarse + fine;
-    if (pitchBendTarget == FFMasterPitchBendTarget::Voice ||
-      pitchBendTarget == FFMasterPitchBendTarget::Osc1 && state.moduleSlot == 0 ||
-      pitchBendTarget == FFMasterPitchBendTarget::Osc2 && state.moduleSlot == 1 ||
-      pitchBendTarget == FFMasterPitchBendTarget::Osc3 && state.moduleSlot == 2 ||
-      pitchBendTarget == FFMasterPitchBendTarget::Osc4 && state.moduleSlot == 3)
-      pitch += pitchBendSemis.Load(s);
+    auto pitch = _key + coarse + fine + voicePitchOffsetSemis.Load(s);
+    if (masterPitchBendTarget == FFMasterPitchBendTarget::Voice ||
+      masterPitchBendTarget == FFMasterPitchBendTarget::Osc1 && state.moduleSlot == 0 ||
+      masterPitchBendTarget == FFMasterPitchBendTarget::Osc2 && state.moduleSlot == 1 ||
+      masterPitchBendTarget == FFMasterPitchBendTarget::Osc3 && state.moduleSlot == 2 ||
+      masterPitchBendTarget == FFMasterPitchBendTarget::Osc4 && state.moduleSlot == 3)
+      pitch += masterPitchBendSemis.Load(s);
 
     auto baseFreq = FBPitchToFreq(pitch);
     basePitchPlain.Store(s, pitch);
