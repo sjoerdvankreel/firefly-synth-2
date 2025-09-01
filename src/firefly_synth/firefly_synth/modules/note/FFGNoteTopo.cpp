711b708e508bca9d3f136b5633d193c02e49cac9
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-09-01 20:20:54 +0200

    take 18

--- src/firefly_synth/firefly_synth/modules/note/FFGNoteTopo.cpp
+++ src/firefly_synth/firefly_synth/modules/note/FFGNoteTopo.cpp
@@ -19,6 +19,32 @@ FFMakeGNoteTopo()
   outputMatrix.slotCount = (int)FBNoteMatrixEntry::Count;
   outputMatrix.id = "{FACE9023-643D-4DE8-B3E9-A9CFCD4B821B}";
   outputMatrix.globalAddr = [](int, int cs, void* state) { return &static_cast<FFProcState*>(state)->dsp.global.gNote.outputNoteMatrix.entries[cs]; };
+  outputMatrix.slotFormatter = [](FBStaticTopo const&, int, int os) {
+    switch ((FBNoteMatrixEntry)os)
+    {
+    case FBNoteMatrixEntry::LastVeloRaw: return "Last Velo Raw";
+    case FBNoteMatrixEntry::LastVeloSmooth: return "Last Velo Smth";
+    case FBNoteMatrixEntry::LowKeyVeloRaw: return "Low Key Velo Raw";
+    case FBNoteMatrixEntry::LowKeyVeloSmooth: return "Low Key Velo Smth";
+    case FBNoteMatrixEntry::HighKeyVeloRaw: return "High Key Velo Raw";
+    case FBNoteMatrixEntry::HighKeyVeloSmooth: return "High Key Velo Smth";
+    case FBNoteMatrixEntry::LowVeloVeloRaw: return "Low Velo VeloRaw";
+    case FBNoteMatrixEntry::LowVeloVeloSmooth: return "Low Velo Velo Smth";
+    case FBNoteMatrixEntry::HighVeloVeloRaw: return "High Velo Velo Raw";
+    case FBNoteMatrixEntry::HighVeloVeloSmooth: return "High Velo Velo Smth";
+    case FBNoteMatrixEntry::LastKeyUntunedRaw: return "Last Key Raw";
+    case FBNoteMatrixEntry::LastKeyUntunedSmooth: return "Last Key Smth";
+    case FBNoteMatrixEntry::LowKeyKeyUntunedRaw: return "Low Key Key Raw";
+    case FBNoteMatrixEntry::LowKeyKeyUntunedSmooth: return "Low Key Key Smth";
+    case FBNoteMatrixEntry::HighKeyKeyUntunedRaw: return "High Key Key Raw";
+    case FBNoteMatrixEntry::HighKeyKeyUntunedSmooth: return "High Key Key Smth";
+    case FBNoteMatrixEntry::LowVeloKeyUntunedRaw: return "Low Velo Key Raw";
+    case FBNoteMatrixEntry::LowVeloKeyUntunedSmooth: return "Low Velo Key Smth";
+    case FBNoteMatrixEntry::HighVeloKeyUntunedRaw: return "High Velo Key Raw";
+    case FBNoteMatrixEntry::HighVeloKeyUntunedSmooth: return "High Velo Key Smth";
+    default: FB_ASSERT(false); return "";
+    }
+  };
 
   return result;
 }
