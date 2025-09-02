deccb6fdc8b4718c6f3dc67cfe844fddb7f05de6
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-09-02 17:53:06 +0200

    take 19

--- src/firefly_synth/firefly_synth/modules/note/FFGNoteTopo.cpp
+++ src/firefly_synth/firefly_synth/modules/note/FFGNoteTopo.cpp
@@ -15,33 +15,33 @@ FFMakeGNoteTopo()
   result->cvOutputs.resize((int)FFGNoteCVOutput::Count);
 
   auto& outputMatrix = result->cvOutputs[(int)FFGNoteCVOutput::NoteMatrix];
-  outputMatrix.name = "MATRIX";
+  outputMatrix.name = "GNote";
   outputMatrix.slotCount = (int)FBNoteMatrixEntry::Count;
   outputMatrix.id = "{FACE9023-643D-4DE8-B3E9-A9CFCD4B821B}";
   outputMatrix.globalAddr = [](int, int cs, void* state) { return &static_cast<FFProcState*>(state)->dsp.global.gNote.outputNoteMatrix.entries[cs]; };
   outputMatrix.slotFormatter = [](FBStaticTopo const&, int, int os) {
     switch ((FBNoteMatrixEntry)os)
     {
-    case FBNoteMatrixEntry::LastVeloRaw: return "Last Velo Raw";
-    case FBNoteMatrixEntry::LastVeloSmooth: return "Last Velo Smth";
-    case FBNoteMatrixEntry::LowKeyVeloRaw: return "Low Key Velo Raw";
-    case FBNoteMatrixEntry::LowKeyVeloSmooth: return "Low Key Velo Smth";
-    case FBNoteMatrixEntry::HighKeyVeloRaw: return "High Key Velo Raw";
-    case FBNoteMatrixEntry::HighKeyVeloSmooth: return "High Key Velo Smth";
-    case FBNoteMatrixEntry::LowVeloVeloRaw: return "Low Velo VeloRaw";
-    case FBNoteMatrixEntry::LowVeloVeloSmooth: return "Low Velo Velo Smth";
-    case FBNoteMatrixEntry::HighVeloVeloRaw: return "High Velo Velo Raw";
-    case FBNoteMatrixEntry::HighVeloVeloSmooth: return "High Velo Velo Smth";
-    case FBNoteMatrixEntry::LastKeyUntunedRaw: return "Last Key Raw";
-    case FBNoteMatrixEntry::LastKeyUntunedSmooth: return "Last Key Smth";
-    case FBNoteMatrixEntry::LowKeyKeyUntunedRaw: return "Low Key Key Raw";
-    case FBNoteMatrixEntry::LowKeyKeyUntunedSmooth: return "Low Key Key Smth";
-    case FBNoteMatrixEntry::HighKeyKeyUntunedRaw: return "High Key Key Raw";
-    case FBNoteMatrixEntry::HighKeyKeyUntunedSmooth: return "High Key Key Smth";
-    case FBNoteMatrixEntry::LowVeloKeyUntunedRaw: return "Low Velo Key Raw";
-    case FBNoteMatrixEntry::LowVeloKeyUntunedSmooth: return "Low Velo Key Smth";
-    case FBNoteMatrixEntry::HighVeloKeyUntunedRaw: return "High Velo Key Raw";
-    case FBNoteMatrixEntry::HighVeloKeyUntunedSmooth: return "High Velo Key Smth";
+    case FBNoteMatrixEntry::LastVeloRaw: return "Last VeloRaw";
+    case FBNoteMatrixEntry::LastVeloSmooth: return "Last VeloSmth";
+    case FBNoteMatrixEntry::LowKeyVeloRaw: return "LowKey VeloRaw";
+    case FBNoteMatrixEntry::LowKeyVeloSmooth: return "LowKey VeloSmth";
+    case FBNoteMatrixEntry::HighKeyVeloRaw: return "HighKey VeloRaw";
+    case FBNoteMatrixEntry::HighKeyVeloSmooth: return "HighKey VeloSmth";
+    case FBNoteMatrixEntry::LowVeloVeloRaw: return "LowVelo VeloRaw";
+    case FBNoteMatrixEntry::LowVeloVeloSmooth: return "LowVelo VeloSmth";
+    case FBNoteMatrixEntry::HighVeloVeloRaw: return "HighVelo VeloRaw";
+    case FBNoteMatrixEntry::HighVeloVeloSmooth: return "HighVelo VeloSmth";
+    case FBNoteMatrixEntry::LastKeyUntunedRaw: return "Last KeyRaw";
+    case FBNoteMatrixEntry::LastKeyUntunedSmooth: return "Last KeySmth";
+    case FBNoteMatrixEntry::LowKeyKeyUntunedRaw: return "LowKey KeyRaw";
+    case FBNoteMatrixEntry::LowKeyKeyUntunedSmooth: return "LowKey KeySmth";
+    case FBNoteMatrixEntry::HighKeyKeyUntunedRaw: return "HighKey KeyRaw";
+    case FBNoteMatrixEntry::HighKeyKeyUntunedSmooth: return "HighKey KeySmth";
+    case FBNoteMatrixEntry::LowVeloKeyUntunedRaw: return "LowVelo KeyRaw";
+    case FBNoteMatrixEntry::LowVeloKeyUntunedSmooth: return "LowVelo KeySmth";
+    case FBNoteMatrixEntry::HighVeloKeyUntunedRaw: return "HighVelo KeyRaw";
+    case FBNoteMatrixEntry::HighVeloKeyUntunedSmooth: return "HighVelo KeySmth";
     default: FB_ASSERT(false); return "";
     }
   };
