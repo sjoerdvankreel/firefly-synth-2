9839fbc7bed84cbed9604b996fa26248139a3a09
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-09-11 22:06:51 +0200

    take 25

--- src/firefly_synth/firefly_synth/modules/mod_matrix/FFModMatrixGraph.cpp
+++ src/firefly_synth/firefly_synth/modules/mod_matrix/FFModMatrixGraph.cpp
@@ -10,8 +10,8 @@ _plugGUI(plugGUI), _type(type) {}
 void
 FFModMatrixGraph::paint(Graphics& g)
 {
-  auto bounds = getBounds().toFloat();
   std::vector<float> yNormalized = {};
+  auto bounds = getBounds().toFloat().reduced(2.0f);
   switch (_type)
   {
   case FFModMatrixGraphType::Source:
@@ -19,13 +19,15 @@ FFModMatrixGraph::paint(Graphics& g)
       yNormalized.push_back(FBToUnipolar(std::sin(2.0f * FBPi * i / (float)bounds.getWidth())));
     break;
   default:
+    yNormalized.push_back(0.5);
+    yNormalized.push_back(0.5);
     break;
   }
 
   Path path;
-  path.startNewSubPath(bounds.getX(), bounds.getY() + bounds.getHeight() * yNormalized[0]);
+  path.startNewSubPath(bounds.getX(), bounds.getY() + bounds.getHeight() * (1.0f - yNormalized[0]));
   for (int i = 1; i < yNormalized.size(); i++)
-    path.lineTo(bounds.getX() + 1.0f, bounds.getY() + bounds.getHeight() * yNormalized[i]);
+    path.lineTo(bounds.getX() + i, bounds.getY() + bounds.getHeight() * (1.0f - yNormalized[i]));
   g.setColour(Colours::white);
   g.strokePath(path, PathStrokeType(1.0f));  
 }
