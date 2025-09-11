b6c0af3958a7a8f6e6f9834f4866090da42162f0
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-09-11 22:10:19 +0200

    take 26

--- src/firefly_synth/firefly_synth/modules/mod_matrix/FFModMatrixGraph.cpp
+++ src/firefly_synth/firefly_synth/modules/mod_matrix/FFModMatrixGraph.cpp
@@ -1,5 +1,6 @@
 #include <firefly_synth/modules/mod_matrix/FFModMatrixGraph.hpp>
 #include <firefly_base/dsp/shared/FBDSPUtility.hpp>
+#include <firefly_base/gui/shared/FBGUI.hpp>
 
 using namespace juce;
 
@@ -10,11 +11,13 @@ _plugGUI(plugGUI), _type(type) {}
 void
 FFModMatrixGraph::paint(Graphics& g)
 {
+  std::string text = {};
   std::vector<float> yNormalized = {};
   auto bounds = getBounds().toFloat().reduced(2.0f);
   switch (_type)
   {
   case FFModMatrixGraphType::Source:
+    text = "Source";
     for (int i = 0; i < bounds.getWidth(); i++)
       yNormalized.push_back(FBToUnipolar(std::sin(2.0f * FBPi * i / (float)bounds.getWidth())));
     break;
@@ -23,6 +26,10 @@ FFModMatrixGraph::paint(Graphics& g)
     yNormalized.push_back(0.5);
     break;
   }
+
+  g.setColour(Colours::darkgrey);
+  g.setFont(FBGUIGetFont().withHeight(16.0f));
+  g.drawText(text, bounds, Justification::centred, false);
 
   Path path;
   path.startNewSubPath(bounds.getX(), bounds.getY() + bounds.getHeight() * (1.0f - yNormalized[0]));
