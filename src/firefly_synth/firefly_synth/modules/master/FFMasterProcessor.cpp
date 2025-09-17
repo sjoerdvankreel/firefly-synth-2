bd56e81b0546c4bf226df25764f0df0635d49e96
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-09-17 22:09:38 +0200

    Reapply "Could this be it? Even if it is, it wasted 2 hours of my life. Now fire up the mac. - take 2!"
    This reverts commit a54df3306d599cd229272c6d065be3a77eaf0f12.

--- src/firefly_synth/firefly_synth/modules/master/FFMasterProcessor.cpp
+++ src/firefly_synth/firefly_synth/modules/master/FFMasterProcessor.cpp
@@ -47,7 +47,7 @@ FFMasterProcessor::Process(FBModuleProcState& state)
     return;
 
   auto& exchangeDSP = exchangeToGUI->global.master[0];
-  exchangeDSP.active = true;
+  exchangeDSP.boolIsActive = 1;
   
   auto& exchangeParams = exchangeToGUI->param.global.master[0];
   exchangeParams.acc.modWheel[0] = params.acc.modWheel[0].Global().Last();
