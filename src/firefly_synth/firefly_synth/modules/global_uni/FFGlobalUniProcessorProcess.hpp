fe3dc2a1fc7811c0feb4553d5fdbe7f434d5c193
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-10-01 22:03:08 +0200

    take 105

--- src/firefly_synth/firefly_synth/modules/global_uni/FFGlobalUniProcessorProcess.hpp
+++ src/firefly_synth/firefly_synth/modules/global_uni/FFGlobalUniProcessorProcess.hpp
@@ -46,34 +46,33 @@ FFGlobalUniProcessor::Apply(
   if (_mode[(int)targetParam] == FFGlobalUniMode::Off)
     return;
 
+  FBSArray<float, 16> modSource;
   auto const* procState = state.ProcAs<FFProcState>();
-  auto const* procStateContainer = state.input->procState;
   if (_mode[(int)targetParam] == FFGlobalUniMode::Auto)
   {
     float voicePos = voiceSlotInGroup / (_voiceCount - 1.0f) - 0.5f;
     auto const& spread = procState->param.global.globalUni[0].acc.autoSpread[(int)targetParam].Global().CV();
     for (int s = 0; s < FBFixedBlockSamples; s += FBSIMDFloatCount)
-    {
-      FBBatch<float> modSource = 0.5f + voicePos * spread.Load(s);
-      targetSignal.Store(s, FFModulateRemap(modSource, 1.0f, targetSignal.Load(s)));
-    }
-  }
-  else
+      modSource.Store(s, 0.5f + voicePos * spread.Load(s));
+  } else
   {
+    auto const* procStateContainer = state.input->procState;
     int manualParamIndex = (int)FFGlobalUniParam::ManualFirst + (int)targetParam;
     auto const* manualParamTopo = state.topo->audio.ParamAtTopo({ { (int)FFModuleType::GlobalUni, 0 }, { manualParamIndex, voiceSlotInGroup } });
     auto const& manualCvNorm = procStateContainer->Params()[manualParamTopo->runtimeParamIndex].GlobalAcc().Global().CV();
-    if (targetParam == FFGlobalUniTarget::VoiceCoarse || targetParam == FFGlobalUniTarget::OscCoarse)
-      for (int s = 0; s < FBFixedBlockSamples; s += FBSIMDFloatCount)
-      {
-        auto modAmtSemisNorm = FBToBipolar(manualCvNorm.Load(s)) * FFGlobalUniCoarseSemis / FFOsciCoarseSemis * 0.5f;
-        targetSignal.Store(s, xsimd::clip(targetSignal.Load(s) + modAmtSemisNorm, FBBatch<float>(0.0f), FBBatch<float>(1.0f)));
-      }
-    else if (targetParam == FFGlobalUniTarget::VMixAmp || targetParam == FFGlobalUniTarget::OscGain)
-      for (int s = 0; s < FBFixedBlockSamples; s += FBSIMDFloatCount)
-        targetSignal.Store(s, FFModulateUPMul(manualCvNorm.Load(s), 1.0f, targetSignal.Load(s)));
-    else
-      for (int s = 0; s < FBFixedBlockSamples; s += FBSIMDFloatCount)
-        targetSignal.Store(s, FFModulateRemap(manualCvNorm.Load(s), 1.0f, targetSignal.Load(s)));
+    manualCvNorm.CopyTo(modSource);
   }
+
+  if (targetParam == FFGlobalUniTarget::VoiceCoarse || targetParam == FFGlobalUniTarget::OscCoarse)
+    for (int s = 0; s < FBFixedBlockSamples; s += FBSIMDFloatCount)
+    {
+      auto modAmtSemisNorm = FBToBipolar(modSource.Load(s)) * FFGlobalUniCoarseSemis / FFOsciCoarseSemis * 0.5f;
+      targetSignal.Store(s, xsimd::clip(targetSignal.Load(s) + modAmtSemisNorm, FBBatch<float>(0.0f), FBBatch<float>(1.0f)));
+    }
+  else if (targetParam == FFGlobalUniTarget::VMixAmp || targetParam == FFGlobalUniTarget::OscGain)
+    for (int s = 0; s < FBFixedBlockSamples; s += FBSIMDFloatCount)
+      targetSignal.Store(s, FFModulateUPMul(modSource.Load(s), 1.0f, targetSignal.Load(s)));
+  else
+    for (int s = 0; s < FBFixedBlockSamples; s += FBSIMDFloatCount)
+      targetSignal.Store(s, FFModulateRemap(modSource.Load(s), 1.0f, targetSignal.Load(s)));
 }
