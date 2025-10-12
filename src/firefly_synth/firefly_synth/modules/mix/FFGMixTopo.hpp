cbfe8d754f007b81c078ec2bb7a6745e7b4f6dbe
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-10-12 14:27:09 +0200

    defaults

--- src/firefly_synth/firefly_synth/modules/mix/FFGMixTopo.hpp
+++ src/firefly_synth/firefly_synth/modules/mix/FFGMixTopo.hpp
@@ -6,7 +6,7 @@
 #include <memory>
 
 struct FBStaticModule;
-std::unique_ptr<FBStaticModule> FFMakeGMixTopo();
+std::unique_ptr<FBStaticModule> FFMakeGMixTopo(bool isFx);
 
 enum class FFGMixParam {
   Amp, LFO5ToAmp, Bal, LFO6ToBal,
