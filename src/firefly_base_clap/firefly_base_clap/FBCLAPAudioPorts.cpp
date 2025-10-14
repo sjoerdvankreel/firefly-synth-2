316647c27b9691a388ec4d1e870b3331985072d5
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-10-14 20:36:43 +0200

    take 27

--- src/firefly_base_clap/firefly_base_clap/FBCLAPAudioPorts.cpp
+++ src/firefly_base_clap/firefly_base_clap/FBCLAPAudioPorts.cpp
@@ -8,24 +8,28 @@ FBCLAPPlugin::implementsAudioPorts() const noexcept
 }
 
 uint32_t 
-FBCLAPPlugin::audioPortsCount(bool isInput) const noexcept
+FBCLAPPlugin::audioPortsCount(bool /*isInput*/) const noexcept
 {
-  return isInput ? 0 : 1;
+  return 1;
 }
 
 bool
 FBCLAPPlugin::audioPortsInfo(uint32_t index, bool isInput, clap_audio_port_info* info) const noexcept 
 {
-  if (isInput)
-    return false;
   if (index != 0)
     return false;
 
   info->id = 0;
-  info->name[0] = 0;
   info->channel_count = 2;
   info->port_type = CLAP_PORT_STEREO;
   info->in_place_pair = CLAP_INVALID_ID;
   info->flags = CLAP_AUDIO_PORT_IS_MAIN;
+
+  std::fill(info->name, info->name + sizeof(info->name), 0);
+  if (isInput)
+    std::strcpy(info->name, "Stereo In");
+  else
+    std::strcpy(info->name, "Stereo Out");
+
   return true;
 }
