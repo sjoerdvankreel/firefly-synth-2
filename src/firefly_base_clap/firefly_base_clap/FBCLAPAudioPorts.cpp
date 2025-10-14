8966d8b4b044cbe6912bf14308e40c270631110d
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-10-14 22:55:57 +0200

    take 35

--- src/firefly_base_clap/firefly_base_clap/FBCLAPAudioPorts.cpp
+++ src/firefly_base_clap/firefly_base_clap/FBCLAPAudioPorts.cpp
@@ -23,13 +23,28 @@ FBCLAPPlugin::audioPortsInfo(uint32_t index, bool isInput, clap_audio_port_info*
   info->channel_count = 2;
   info->port_type = CLAP_PORT_STEREO;
   info->in_place_pair = CLAP_INVALID_ID;
-  info->flags = CLAP_AUDIO_PORT_IS_MAIN;
 
   std::fill(info->name, info->name + sizeof(info->name), 0);
-  if (isInput)
-    std::strcpy(info->name, "Stereo In");
-  else
+  if (!isInput)
+  {
+    info->flags = CLAP_AUDIO_PORT_IS_MAIN;
     std::strcpy(info->name, "Stereo Out");
+  }
+  else
+  {
+    // The bustype is important to bitwig.
+    // With main, it wont accept sidechaining.
+    if (_topo->static_->meta.isFx)
+    {
+      info->flags = CLAP_AUDIO_PORT_IS_MAIN;
+      std::strcpy(info->name, "Stereo In");
+    }
+    else
+    {
+      info->flags = 0;
+      std::strcpy(info->name, "Sidechain");
+    }
+  }
 
   return true;
 }
