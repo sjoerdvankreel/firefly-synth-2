948c216504b7533f33e06565232eb23b81a7f0fa
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-10-26 15:59:01 +0100

    take 141

--- src/firefly_synth/firefly_synth/gui/FFTweakGUI.cpp
+++ src/firefly_synth/firefly_synth/gui/FFTweakGUI.cpp
@@ -22,10 +22,10 @@ Component*
 FFMakeTweakGUI(FBPlugGUI* plugGUI)
 {
   FB_LOG_ENTRY_EXIT();
-  auto grid = plugGUI->StoreComponent<FBGridComponent>(true, std::vector<int> { 1 }, std::vector<int> { 0, 0, 0 });
-  grid->Add(0, 0, plugGUI->StoreComponent<FBLastTweakedLabel>(plugGUI));
-  grid->Add(0, 1, plugGUI->StoreComponent<FBLastTweakedTextBox>(plugGUI, 80));
-  grid->Add(0, 2, plugGUI->StoreComponent<FBFillerComponent>(3, 1));
+  auto grid = plugGUI->StoreComponent<FBGridComponent>(true, std::vector<int> { 1 }, std::vector<int> { 0, 1, 0 });
+  grid->Add(0, 0, plugGUI->StoreComponent<FBAutoSizeLabel>("Last Tweaked"));
+  grid->Add(0, 1, plugGUI->StoreComponent<FBLastTweakedLabel>(plugGUI));
+  grid->Add(0, 2, plugGUI->StoreComponent<FBLastTweakedTextBox>(plugGUI, 100));
   grid->MarkSection({ { 0, 0 }, { 1, 3 } });
   auto subSection = plugGUI->StoreComponent<FBSubSectionComponent>(grid);
   return plugGUI->StoreComponent<FBSectionComponent>(subSection);
