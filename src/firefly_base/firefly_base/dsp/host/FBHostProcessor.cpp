a96e9fd31f25ef576d649f11f0e185485200eb02
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-10-30 18:15:38 +0100

    cleanup

--- src/firefly_base/firefly_base/dsp/host/FBHostProcessor.cpp
+++ src/firefly_base/firefly_base/dsp/host/FBHostProcessor.cpp
@@ -1,3 +1,4 @@
+#include <firefly_base/base/topo/static/FBStaticTopo.hpp>
 #include <firefly_base/base/topo/runtime/FBRuntimeTopo.hpp>
 #include <firefly_base/base/state/proc/FBAccParamState.hpp>
 #include <firefly_base/base/state/proc/FBVoiceAccParamState.hpp>
