6d8facccae100c300bfd34c694c427231ce8dc55
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-11-19 21:01:30 +0100

    take 48

--- src/firefly_base/firefly_base/gui/components/FBMSEGEditor.cpp
+++ src/firefly_base/firefly_base/gui/components/FBMSEGEditor.cpp
@@ -102,7 +102,7 @@ FBMSEGEditor::mouseDrag(MouseEvent const& event)
     double xBefore = _dragIndex == 0 ? _initPointScreen.getX() : _currentPointsScreen[_dragIndex - 1].getX();
     double segLen = xCurrent - xBefore;
     double dragLen = adjustedPosition.x - xBefore;
-    double xPos = std::max(dragLen / segLen, 0.0);
+    double xPos = dragLen / segLen;
     _model.points[_dragIndex].lengthReal *= xPos;
     _model.points[_dragIndex].lengthReal = std::clamp(_model.points[_dragIndex].lengthReal, 0.0, _maxLengthReal);
   }
