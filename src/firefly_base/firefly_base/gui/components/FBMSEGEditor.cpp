06326837329d78cd7ecd57568620f6d925f6b922
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-11-23 13:57:46 +0100

    Revert "take 3"
    This reverts commit c1bfd29729f6e1cfc30db9494af98c23003ac8e3.

--- src/firefly_base/firefly_base/gui/components/FBMSEGEditor.cpp
+++ src/firefly_base/firefly_base/gui/components/FBMSEGEditor.cpp
@@ -17,43 +17,14 @@ FBMSEGEditor(
   int maxPoints,
   int maxLengthRatioNum,
   int maxLengthRatioDen,
-  double maxLengthReal,
-  std::vector<int> const& snapXCounts,
-  std::vector<int> const& snapYCounts):
+  double maxLengthReal):
 Component(),
 _plugGUI(plugGUI),
 _name(name),
 _maxPoints(maxPoints),
 _maxLengthRatioNum(maxLengthRatioNum),
 _maxLengthRatioDen(maxLengthRatioDen),
-_maxLengthReal(maxLengthReal),
-_snapXCounts(snapXCounts),
-_snapYCounts(snapYCounts)
-{
-  PopupMenu snapXMenu;
-  for (int i = 0; i < _snapXCounts.size(); i++)
-    snapXMenu.addItem(i + 1, std::to_string(_snapXCounts[i]));
-  PopupMenu snapYMenu;
-  for (int i = 0; i < _snapYCounts.size(); i++)
-    snapYMenu.addItem(i + 1, std::to_string(_snapYCounts[i]));
-
-  _snapXLabel = std::make_unique<FBAutoSizeLabel>("Snap X");
-  _snapXToggle = std::make_unique<FBAutoSizeToggleButton>();
-  _snapXCombo = std::make_unique<FBAutoSizeComboBox>(snapXMenu);
-  _snapYLabel = std::make_unique<FBAutoSizeLabel>("Snap Y");
-  _snapYToggle = std::make_unique<FBAutoSizeToggleButton>();
-  _snapYCombo = std::make_unique<FBAutoSizeComboBox>(snapYMenu);
-  _topGrid = std::make_unique<FBGridComponent>(true, -1, -1, std::vector<int> { { 1 } }, std::vector<int> { 0, 0, 0, 0, 1 });
-  _topGrid->Add(0, 0, _snapXLabel.get());
-  _topGrid->Add(0, 1, _snapXToggle.get());
-  _topGrid->Add(0, 2, _snapXCombo.get());
-  _topGrid->Add(0, 3, _snapYLabel.get());
-  _topGrid->Add(0, 4, _snapYToggle.get());
-  _topGrid->Add(0, 5, _snapYCombo.get());
-  _grid = std::make_unique<FBGridComponent>(true, -1, -1, std::vector<int> { 0, 1 }, std::vector<int> { 0 });
-  _grid->Add(0, 0, _topGrid.get());
-
-}
+_maxLengthReal(maxLengthReal) {}
 
 void
 FBMSEGEditor::UpdateModel()
