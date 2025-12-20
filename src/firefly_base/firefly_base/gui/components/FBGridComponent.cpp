c610bdb9588f37b3a6716979c7c2a0342b99d109
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-12-20 23:47:12 +0100

    take 115

--- src/firefly_base/firefly_base/gui/components/FBGridComponent.cpp
+++ src/firefly_base/firefly_base/gui/components/FBGridComponent.cpp
@@ -255,7 +255,7 @@ FBGridComponent::paint(Graphics& g)
     }
     if (_sectionsAndMarks[i].mark == FBGridSectionMark::AlternateAndAlternate)
     {
-      g.setColour(FBGetLookAndFeel()->FindColorSchemeFor(*this).paramPrimary.darker(4.0f));
+      g.setColour(FBGetLookAndFeel()->FindColorSchemeFor(*this).paramPrimary.darker(2.0f));
       g.fillRoundedRectangle(x0, y0, x1 - x0 - subtractR, y1 - y0, 3.0f);
     }
     if (_sectionsAndMarks[i].mark == FBGridSectionMark::Alternate ||
