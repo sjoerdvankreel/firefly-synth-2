0fe1249c74532f9b43f66e67df83f2534531d8ae
sjoerdvankreel <sjoerdvankreel@gmail.com>
2025-12-22 11:31:32 +0100

    take 143

--- src/firefly_base/firefly_base/gui/shared/FBLookAndFeel.cpp
+++ src/firefly_base/firefly_base/gui/shared/FBLookAndFeel.cpp
@@ -304,86 +304,73 @@ FBLookAndFeel::drawPopupMenuItem(
   const bool isHighlighted, const bool isTicked,
   const bool hasSubMenu, const juce::String& text,
   const String& shortcutKeyText,
-  const Drawable* icon, const Colour* const textColourToUse)
+  const Drawable* /*icon*/, const Colour* const textColourToUse)
 {
+  auto const& scheme = Theme().defaultColorScheme;
+  Colour textColor = scheme.text;
+  Colour selectedColor = textColourToUse != nullptr ? *textColourToUse : scheme.text;
+
   if (isSeparator)
   {
     auto r = area.reduced(5, 0);
     r.removeFromTop(roundToInt(((float)r.getHeight() * 0.5f) - 0.5f));
-
-    g.setColour(findColour(PopupMenu::textColourId).withAlpha(0.3f));
+    g.setColour(selectedColor);
     g.fillRect(r.removeFromTop(1));
+    return;
+  }
+
+  auto r = area.reduced(1);
+  if (isHighlighted && isActive)
+  {
+    g.setColour(scheme.sectionBackground);
+    g.fillRect(r);
+    g.setColour(selectedColor);
   }
   else
   {
-    auto textColour = (textColourToUse == nullptr ? findColour(PopupMenu::textColourId)
-      : *textColourToUse);
+    g.setColour(textColor.withMultipliedAlpha(isActive ? 1.0f : 0.5f));
+  }
 
-    auto r = area.reduced(1);
+  r.reduce(jmin(5, area.getWidth() / 20), 0);
+  auto font = getPopupMenuFont();
+  auto maxFontHeight = (float)r.getHeight() / 1.3f;
+  if (font.getHeight() > maxFontHeight)
+    font.setHeight(maxFontHeight);
 
-    if (isHighlighted && isActive)
-    {
-      g.setColour(findColour(PopupMenu::highlightedBackgroundColourId));
-      g.fillRect(r);
+  g.setFont(font.withHeight(FBGUIGetPopupMenuFontHeightFloat()));
+  auto iconArea = r.removeFromLeft(roundToInt(maxFontHeight)).toFloat();
+  if (isTicked)
+  {
+    auto tick = getTickShape(1.0f);
+    g.fillPath(tick, tick.getTransformToScaleToFit(iconArea.reduced(iconArea.getWidth() / 5, 0).toFloat(), true));
+  }
 
-      g.setColour(findColour(PopupMenu::highlightedTextColourId));
-    }
-    else
-    {
-      g.setColour(textColour.withMultipliedAlpha(isActive ? 1.0f : 0.5f));
-    }
+  if (hasSubMenu)
+  {
+    auto arrowH = 0.6f * getPopupMenuFont().getAscent();
 
-    r.reduce(jmin(5, area.getWidth() / 20), 0);
+    auto x = static_cast<float> (r.removeFromRight((int)arrowH).getX());
+    auto halfH = static_cast<float> (r.getCentreY());
 
-    auto font = getPopupMenuFont();
+    Path path;
+    path.startNewSubPath(x, halfH - arrowH * 0.5f);
+    path.lineTo(x + arrowH * 0.6f, halfH);
+    path.lineTo(x, halfH + arrowH * 0.5f);
 
-    auto maxFontHeight = (float)r.getHeight() / 1.3f;
+    g.strokePath(path, PathStrokeType(2.0f));
+  }
 
-    if (font.getHeight() > maxFontHeight)
-      font.setHeight(maxFontHeight);
+  r.removeFromRight(3);
+  g.drawFittedText(text, r, Justification::centredLeft, 1);
 
-    g.setFont(font.withHeight(FBGUIGetPopupMenuFontHeightFloat()));
+  if (shortcutKeyText.isNotEmpty())
+  {
+    auto f2 = font;
+    f2.setHeight(f2.getHeight() * 0.75f);
+    f2.setHorizontalScale(0.95f);
+    g.setFont(f2);
 
-    auto iconArea = r.removeFromLeft(roundToInt(maxFontHeight)).toFloat();
-
-    if (icon != nullptr)
-    {
-      icon->drawWithin(g, iconArea, RectanglePlacement::centred | RectanglePlacement::onlyReduceInSize, 1.0f);
-      r.removeFromLeft(roundToInt(maxFontHeight * 0.5f));
-    }
-    else if (isTicked)
-    {
-      auto tick = getTickShape(1.0f);
-      g.fillPath(tick, tick.getTransformToScaleToFit(iconArea.reduced(iconArea.getWidth() / 5, 0).toFloat(), true));
-    }
-
-    if (hasSubMenu)
-    {
-      auto arrowH = 0.6f * getPopupMenuFont().getAscent();
-
-      auto x = static_cast<float> (r.removeFromRight((int)arrowH).getX());
-      auto halfH = static_cast<float> (r.getCentreY());
-
-      Path path;
-      path.startNewSubPath(x, halfH - arrowH * 0.5f);
-      path.lineTo(x + arrowH * 0.6f, halfH);
-      path.lineTo(x, halfH + arrowH * 0.5f);
-
-      g.strokePath(path, PathStrokeType(2.0f));
-    }
-
-    r.removeFromRight(3);
-    g.drawFittedText(text, r, Justification::centredLeft, 1);
-
-    if (shortcutKeyText.isNotEmpty())
-    {
-      auto f2 = font;
-      f2.setHeight(f2.getHeight() * 0.75f);
-      f2.setHorizontalScale(0.95f);
-      g.setFont(f2);
-
-      g.drawText(shortcutKeyText, r, Justification::centredRight, true);
-    }
+    g.drawText(shortcutKeyText, r, Justification::centredRight, true);
   }
 }
 
