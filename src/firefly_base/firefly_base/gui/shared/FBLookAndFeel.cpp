cbdad71fe308e26bdc29a55e064d9af478587c0a
sjoerdvankreel <sjoerdvankreel@gmail.com>
2026-01-01 22:06:54 +0100

    take 16

--- src/firefly_base/firefly_base/gui/shared/FBLookAndFeel.cpp
+++ src/firefly_base/firefly_base/gui/shared/FBLookAndFeel.cpp
@@ -278,7 +278,7 @@ FBLookAndFeel::layoutFileBrowserComponent(
 {
   auto buttonWidth = 50;
   auto b = browserComp.getLocalBounds();
-  auto topSlice = b.removeFromTop(22);
+  auto topSlice = b.removeFromTop(32);
   auto bottomSlice = b.removeFromBottom(28);
   currentPathBox->setBounds(topSlice.removeFromLeft(topSlice.getWidth() - buttonWidth));
   topSlice.removeFromLeft(6);
