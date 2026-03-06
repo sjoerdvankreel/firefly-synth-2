583857b6bdb3283620fa6c296019e54b533df3c1
sjoerdvankreel <sjoerdvankreel@gmail.com>
2026-03-06 19:09:57 +0100

    take 188

--- src/firefly_synth/firefly_synth/modules/osci/FFOsciGUI.cpp
+++ src/firefly_synth/firefly_synth/modules/osci/FFOsciGUI.cpp
@@ -396,8 +396,7 @@ MakeOsciTab(FBPlugGUI* plugGUI, int moduleSlot)
   auto grid = plugGUI->StoreComponent<FBGridComponent>(plugGUI, true, std::vector<int> { 1 }, std::vector<int> { 0, 0 });
   grid->Add(0, 0, MakeOsciSectionMain(plugGUI, moduleSlot));
   grid->Add(0, 1, MakeOsciSectionUni(plugGUI, moduleSlot));
-  auto margin = plugGUI->StoreComponent<FBMarginComponent>(plugGUI, true, true, true, true, grid);
-  return plugGUI->StoreComponent<FBModuleComponent>(plugGUI->HostContext()->Topo(), (int)FFModuleType::Osci, moduleSlot, margin);
+  return plugGUI->StoreComponent<FBModuleComponent>(plugGUI->HostContext()->Topo(), (int)FFModuleType::Osci, moduleSlot, grid);
 }
 
 static Component*
