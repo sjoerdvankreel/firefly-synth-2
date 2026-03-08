5fc2debb87d83833b47e97bec2cac2bec0dbad9b
sjoerdvankreel <sjoerdvankreel@gmail.com>
2026-03-08 21:38:11 +0100

    take 225

--- src/firefly_synth/firefly_synth/modules/osci_mod/FFOsciModGUI.cpp
+++ src/firefly_synth/firefly_synth/modules/osci_mod/FFOsciModGUI.cpp
@@ -24,13 +24,11 @@ FFMakeOsciModGUISectionMain(FBPlugGUI* plugGUI)
   auto topo = plugGUI->HostContext()->Topo();
   auto grid = plugGUI->StoreComponent<FBGridComponent>(plugGUI, true, std::vector<int> { 1, 1 }, std::vector<int> { 1, 0 });
   auto oversample = topo->audio.ParamAtTopo({ { (int)FFModuleType::OsciMod, 0 }, { (int)FFOsciModParam::Oversample, 0 } });
-  grid->Add(0, 0, plugGUI->StoreComponent<FBAutoSizeLabel>(plugGUI, "OvrSmp", FBLabelAlign::Left, FBLabelColors::PrimaryForeground));
+  grid->Add(0, 0, plugGUI->StoreComponent<FBParamLabel>(plugGUI, oversample));
   grid->Add(0, 1, plugGUI->StoreComponent<FBParamToggleButton>(plugGUI, oversample));
   auto expoFM = topo->audio.ParamAtTopo({ { (int)FFModuleType::OsciMod, 0 }, { (int)FFOsciModParam::ExpoFM, 0 } });
   grid->Add(1, 0, plugGUI->StoreComponent<FBParamLabel>(plugGUI, expoFM));
   grid->Add(1, 1, plugGUI->StoreComponent<FBParamToggleButton>(plugGUI, expoFM));
-  grid->MarkSection({ { 0, 0 }, { 1, 1 } }, FBGridSectionMark::DefaultBackground);
-  grid->MarkSection({ { 1, 0 }, { 1, 1 } }, FBGridSectionMark::DefaultBackground);
   return plugGUI->StoreComponent<FBCardComponent>(plugGUI, grid);
 }
 
