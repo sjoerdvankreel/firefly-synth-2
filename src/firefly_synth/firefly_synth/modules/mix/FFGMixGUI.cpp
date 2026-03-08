63b14af536afda20650d002caaf5a1cfc0a7256e
sjoerdvankreel <sjoerdvankreel@gmail.com>
2026-03-08 21:22:54 +0100

    take 223

--- src/firefly_synth/firefly_synth/modules/mix/FFGMixGUI.cpp
+++ src/firefly_synth/firefly_synth/modules/mix/FFGMixGUI.cpp
@@ -108,7 +108,7 @@ MakeGMixGUISectionAmpBal(FBPlugGUI* plugGUI)
   auto topo = plugGUI->HostContext()->Topo();
   auto grid = plugGUI->StoreComponent<FBGridComponent>(plugGUI, true, std::vector<int> { 1, 1 }, std::vector<int> { 0, 1, 0, 0 });
   auto amp = topo->audio.ParamAtTopo({ { (int)FFModuleType::GMix, 0}, { (int)FFGMixParam::Amp, 0}});
-  grid->Add(0, 0, plugGUI->StoreComponent<FBParamLabel>(plugGUI, amp));
+  grid->Add(0, 0, plugGUI->StoreComponent<FBAutoSizeLabel>(plugGUI, "Amp", FBLabelAlign::Left, FBLabelColors::PrimaryForeground));
   grid->Add(0, 1, plugGUI->StoreComponent<FBParamSlider>(plugGUI, amp, Slider::SliderStyle::LinearHorizontal));
   auto lfo5ToAmp = topo->audio.ParamAtTopo({ { (int)FFModuleType::GMix, 0}, { (int)FFGMixParam::LFO5ToAmp, 0} });
   grid->Add(0, 2, plugGUI->StoreComponent<FBParamLabel>(plugGUI, lfo5ToAmp));
@@ -119,6 +119,7 @@ MakeGMixGUISectionAmpBal(FBPlugGUI* plugGUI)
   auto lfo6ToBal = topo->audio.ParamAtTopo({ { (int)FFModuleType::GMix, 0 }, { (int)FFGMixParam::LFO6ToBal, 0 } });
   grid->Add(1, 2, plugGUI->StoreComponent<FBParamLabel>(plugGUI, lfo6ToBal));
   grid->Add(1, 3, plugGUI->StoreComponent<FBParamSlider>(plugGUI, lfo6ToBal, Slider::SliderStyle::RotaryVerticalDrag));
+  grid->MarkSection({ { 0, 0 }, { 1, 1 } }, FBGridSectionMark::DefaultBackground);
   return plugGUI->StoreComponent<FBCardComponent>(plugGUI, grid);
 }
 
